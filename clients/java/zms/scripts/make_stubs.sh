319a5d314a7430e0a4de1990693b6684bb2c477e
Henry Avetisyan <hga@yahoo-inc.com>
2017-03-12 14:50:30 -0700

    Skip automated rdl code generation during travis builds

--- clients/java/zms/scripts/make_stubs.sh
+++ clients/java/zms/scripts/make_stubs.sh
@@ -2,19 +2,29 @@
 
 # If the zms_core dependency has been updated, then this script should be run
 # manually to pick up the latest rdl to generate the appropriate client library
+# however, we're not going to run this utility during our automated builds since
+# builds must be done based on files already checked-in into git
+
+if [ ! -z "${TRAVIS_PULL_REQUEST}" ] || [ ! -z "${TRAVIS_TAG}" ]; then
+    echo >&2 "------------------------------------------------------------------------";
+    echo >&2 "SOURCE NOTICE";
+    echo >&2 "------------------------------------------------------------------------";
+    echo >&2 "Automated Build. Skipping source generation...";
+    exit 0;
+fi
 
 # Note this script is dependent on the rdl utility.
 # go get github.com/ardielle/ardielle-tools/...
 
 if [ -x "$(command -v go)" ]; then
-    go get github.com/ardielle/ardielle-tools/...
+    go get -u github.com/ardielle/ardielle-tools/...
 fi
 
 command -v rdl >/dev/null 2>&1 || {
     echo >&2 "------------------------------------------------------------------------";
     echo >&2 "SOURCE WARNING";
     echo >&2 "------------------------------------------------------------------------";
-    echo >&2 "Please install rdl utility: go get github.com/ardielle/ardielle-tools/...";
+    echo >&2 "Please install rdl utility: go get -u github.com/ardielle/ardielle-tools/...";
     echo >&2 "Skipping source generation...";
     exit 0;
 }
