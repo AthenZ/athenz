e8e16cfa76a2fc84f4746c5fe4a23282d766918e
Charlie Kim <charlesk@yahoo-inc.com>
2017-05-09 16:16:26 -0700

    tag image

--- docker/publish_docker.sh
+++ docker/publish_docker.sh
@@ -7,6 +7,9 @@ echo "-----------------------------------------------"
 
 docker build -t athenz/athenz .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+TRAVIS_TAG=1.7.10-SNAPSHOT
+echo "tagging Athenz Docker image with tag: $TRAVIS_TAG"
+docker tag athenz/athenz athenz/athenz:$TRAVIS_TAG
 docker push athenz/athenz
 
 echo "-----------------------------------------------"
