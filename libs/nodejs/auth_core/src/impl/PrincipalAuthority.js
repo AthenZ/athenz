a49f140b3c3f76d9a5c462d23765eee8e3a3675b
Tatsuya Yano <tatyano@yahoo-corp.jp>
2017-05-18 18:33:19 +0900

    Added auth_core Node.js version and integrated to UI

---
+++ libs/nodejs/auth_core/src/impl/PrincipalAuthority.js
@@ -0,0 +1,261 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+var winston = require('winston');
+var PrincipalToken = require('../token/PrincipalToken');
+var SimplePrincipal = require('./SimplePrincipal');
+var config = require('../../config/config')();
+winston.level = config.loglevel;
+
+var USER_DOMAIN = 'user';
+var SYS_AUTH_DOMAIN = 'sys.auth';
+var ZMS_SERVICE = 'zms';
+var ZTS_SERVICE = 'zts';
+
+var ATHENZ_PROP_TOKEN_OFFSET = Number(config.principalTokenAllowedOffset);
+var ATHENZ_PROP_IP_CHECK_MODE = config.principalIpCheckMode;
+var ATHENZ_PROP_USER_DOMAIN = config.principalUserDomain;
+var ATHENZ_PROP_PRINCIPAL_HEADER = config.principalHeader;
+
+class PrincipalAuthority {
+  constructor() {
+    this._keyStore = null;
+    this._allowedOffset = (ATHENZ_PROP_TOKEN_OFFSET) ? Number(ATHENZ_PROP_TOKEN_OFFSET) : 300;
+    this._ipCheckMode = ATHENZ_PROP_IP_CHECK_MODE || 'OPS_WRITE';
+    this._userDomain = ATHENZ_PROP_USER_DOMAIN || 'user';
+    this._headerName = ATHENZ_PROP_PRINCIPAL_HEADER || 'Athenz-Principal-Auth';
+
+    // case of invalid value, we'll default back to 5 minutes
+    if (this._allowedOffset < 0) {
+      this._allowedOffset = 300;
+    }
+  }
+
+  initialize() {
+  }
+
+  getDomain() {
+    return null;
+  }
+
+  getHeader() {
+    return this._headerName;
+  }
+
+  authenticate(signedToken, remoteAddr, httpMethod) {
+    winston.debug('Authenticating PrincipalToken: ' + signedToken);
+
+    var serviceToken = null;
+    try {
+      serviceToken = new PrincipalToken(signedToken);
+    } catch (e) {
+      winston.error('PrincipalAuthority:authenticate: Invalid token: exc=' + e.message +
+        ' : credential=' + PrincipalToken.getUnsignedToken(signedToken));
+      return null;
+    }
+
+    /* before authenticating verify that if this is a valid
+     * authorized service token or not and if required
+     * components are provided (the method already logs
+     * all error messages) */
+    if (!serviceToken.isValidAuthorizedServiceToken()) {
+      winston.error('PrincipalAuthority:authenticate: Invalid authorized service token: credential=' +
+        PrincipalToken.getUnsignedToken(signedToken));
+      return null;
+    }
+
+    var tokenDomain = serviceToken.getDomain().toString().toLowerCase();
+    var tokenName = serviceToken.getName().toString().toLowerCase();
+    var keyService = serviceToken.getKeyService();
+    var userToken = (tokenDomain === this._userDomain);
+
+    /* get the public key for this token to validate signature */
+    var publicKey = this._getPublicKey(tokenDomain, tokenName, keyService, serviceToken.getKeyId(), userToken);
+
+    /* the validate method logs all error messages */
+    var writeOp = this._isWriteOperation(httpMethod);
+    if (serviceToken.validate(publicKey, this._allowedOffset, !writeOp) === false) {
+      winston.error('PrincipalAuthority:authenticate: service token validation failure: credential=' +
+        PrincipalToken.getUnsignedToken(signedToken));
+      return null;
+    }
+
+    /* if an authorized service signature is available then we're going to validate
+     * that signature as well to support token chaining in Athenz and, if necessary,
+     * bypass IP address mismatch for users */
+    var authorizedServiceName = null;
+    if (serviceToken.getAuthorizedServiceSignature()) {
+      authorizedServiceName = this._validateAuthorizeService(serviceToken);
+      if (!authorizedServiceName) {
+        winston.error('PrincipalAuthority:authenticate: validation of authorized service failure: credential=' +
+          PrincipalToken.getUnsignedToken(signedToken));
+        return null;
+      }
+    }
+
+    /* if we have a usertoken and our remote ip check enabled, verify that the IP address
+     * matches before allowing the operation go through */
+    if (userToken && !this._remoteIpCheck(remoteAddr, writeOp, serviceToken, authorizedServiceName)) {
+      winston.error('PrincipalAuthority:authenticate: IP Mismatch - token (' + serviceToken.getIP() +
+        ') request (' + remoteAddr + ')');
+      return null;
+    }
+
+    /* all the role members in Athenz are normalized to lower case so we need to make
+     * sure our principal's name and domain are created with lower case as well */
+    var princ = SimplePrincipal.createByUserIdentity(tokenDomain, tokenName, signedToken, serviceToken.getTimestamp(), this);
+    princ.setUnsignedCreds(serviceToken.getUnsignedToken());
+    princ.setAuthorizedService(authorizedServiceName);
+    princ.setOriginalRequestor(serviceToken.getOriginalRequestor());
+    princ.setKeyService(keyService);
+    princ.setIP(serviceToken.getIP());
+    princ.setKeyId(serviceToken.getKeyId());
+    return princ;
+  }
+
+  _remoteIpCheck(remoteAddr, writeOp, serviceToken, authorizedServiceName) {
+    var checkResult = true;
+    switch (this._ipCheckMode) {
+      case 'OPS_ALL':
+        if (remoteAddr !== serviceToken.getIP()) {
+          checkResult = false;
+        }
+        break;
+      case 'OPS_WRITE':
+        /* if we have a user token for a write operation and we have an IP address
+         * mismatch then we'll allow this authenticate request to proceed only if it's
+         * been configured with authorized user only. */
+        if (writeOp && remoteAddr !== serviceToken.getIP()) {
+          if (!authorizedServiceName) {
+            checkResult = false;
+          }
+        }
+        break;
+      case 'OPS_NONE':
+        break;
+      default:
+        checkResult = false;
+        break;
+    }
+    return checkResult;
+  }
+
+  _getPublicKey(tokenDomain, tokenName, keyService, keyId, userToken) {
+    /* by default we're going to look for the public key for the domain
+     * and service defined in the token */
+    var publicKeyDomain = tokenDomain;
+    var publicKeyService = tokenName;
+
+    /* now let's handle the exceptions:
+     * 1) if the token has a key service field set then only supported values are
+     * either zms or zts, so we use sys.auth.zms or sys.auth.zts services
+     * 2) if the token's domain is user then it's a user token or if it's sd then
+     * it's our special project token so for those cases we are going to ask for
+     * zms's own public key. */
+    if (keyService) {
+      if (keyService === ZMS_SERVICE) {
+        publicKeyDomain = SYS_AUTH_DOMAIN;
+        publicKeyService = ZMS_SERVICE;
+      } else if (keyService === ZTS_SERVICE) {
+        publicKeyDomain = SYS_AUTH_DOMAIN;
+        publicKeyService = ZTS_SERVICE;
+      }
+    } else if (userToken) {
+      publicKeyDomain = SYS_AUTH_DOMAIN;
+      publicKeyService = ZMS_SERVICE;
+    }
+
+    return this._keyStore.getPublicKey(publicKeyDomain, publicKeyService, keyId);
+  }
+
+  _isWriteOperation(httpMethod) {
+    if (!httpMethod) {
+      return false;
+    }
+    if (httpMethod.toString().toUpperCase() === 'PUT' || httpMethod.toString().toUpperCase() === 'POST' || httpMethod.toString().toUpperCase() === 'DELETE') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  _getAuthorizedServiceName(authorizedServices, authorizedServiceName) {
+    /* if we have an authorized service name specified then it must be
+     * present in the authorized services list or if it's null then the
+     * list must contain a single element only */
+    var serviceName = authorizedServiceName;
+    var err = null;
+    if (!serviceName) {
+      if (authorizedServices.length !== 1) {
+        err = new Error('getAuthorizedServiceName() failed: No authorized service name specified');
+        winston.error(err);
+        return null;
+      }
+      serviceName = authorizedServices[0];
+    } else {
+      if (authorizedServices.indexOf(serviceName) === -1) {
+        err = new Error('getAuthorizedServiceName() failed: Invalid authorized service name specified:' + serviceName);
+        winston.error(err);
+        return null;
+      }
+    }
+    return serviceName;
+  }
+
+  _validateAuthorizeService(userToken) {
+    /* if we have an authorized service name specified then it must be
+     * present in the authorized services list or if it's null then the
+     * list must contain a single element only */
+    var authorizedServiceName = userToken.getAuthorizedServiceName();
+    var err = null;
+    if (!authorizedServiceName) {
+      var authorizedServices = userToken.getAuthorizedServices();
+      if (!authorizedServices || authorizedServices.length !== 1) {
+        err = new Error('PrincipalAuthority:validateAuthorizeService: ' +
+          'No service name and services list empty OR contains multiple entries: token=' + userToken.getUnsignedToken());
+        winston.error(err);
+        return null;
+      } else {
+        authorizedServiceName = authorizedServices[0];
+      }
+    }
+
+    /* need to extract domain and service name from our full service name value */
+    var idx = authorizedServiceName.lastIndexOf('.');
+    if (idx <= 0 || idx === authorizedServiceName.length - 1) {
+      err = new Error('PrincipalAuthority:validateAuthorizeService: ' +
+        'failed: token=' + userToken.getUnsignedToken() +
+        ' : Invalid authorized service name specified=' + authorizedServiceName);
+      winston.error(err);
+      return null;
+    }
+
+    var publicKey = this._keyStore.getPublicKey(authorizedServiceName.substring(0, idx), authorizedServiceName.substring(idx + 1), userToken.getAuthorizedServiceKeyId());
+
+    /* the token method reports all error messages */
+    if (!userToken.validateForAuthorizedService(publicKey)) {
+      err = new Error('PrincipalAuthority:validateAuthorizeService: token validation for authorized service failed');
+      winston.error(err);
+      return null;
+    }
+    return authorizedServiceName;
+  }
+
+  setKeyStore(keyStore) {
+    this._keyStore = keyStore;
+  }
+}
+
+module.exports = PrincipalAuthority;
---
+++ libs/nodejs/auth_core/src/token/Token.js
@@ -0,0 +1,185 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+var winston = require('winston');
+var crypto = require('../util/Crypto');
+var config = require('../../config/config')();
+winston.level = config.loglevel;
+
+var ATHENZ_TOKEN_MAX_EXPIRY = (Number(config.tokenMaxExpiry) > 30 * 24 * 60 * 60) ? 30 * 24 * 60 * 60 : Number(config.tokenMaxExpiry);
+var ATHENZ_TOKEN_NO_EXPIRY = config.tokenNoExpiry;
+
+class Token {
+  constructor() {
+    this._unsignedToken = null;
+    this._signedToken = null;
+    this._version = null;
+    this._salt = null;
+    this._host = null;
+    this._ip = null;
+    this._domain = null;
+    this._signature = null;
+    this._keyId = '0';
+    this._expiryTime = 0;
+    this._timestamp = 0;
+    this._digestAlgorithm = 'SHA256';
+  }
+
+  sign(privateKey) {
+    try {
+      this._signature = crypto.sign(this._unsignedToken, privateKey, this._digestAlgorithm);
+      this._signedToken = this._unsignedToken + ';s=' + this._signature;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  setTimeStamp(issueTime, expirationWindow) {
+    this._timestamp = (issueTime > 0) ? issueTime : Math.floor(Date.now() / 1000);
+    this._expiryTime = this._timestamp + expirationWindow;
+  }
+
+  /*eslint complexity: ["error", 12]*/
+  validate(publicKey, allowedOffset, allowNoExpiry) {
+    var err = null;
+    if (!this._unsignedToken || !this._signature) {
+      err = new Error('Token:validate: token=' + this._unsignedToken +
+        ' : missing data/signature component');
+      winston.error(err);
+      return false;
+    }
+
+    if (!publicKey) {
+      err = new Error('Token:validate: token=' + this._unsignedToken +
+        ' : No public key provided');
+      winston.error(err);
+      return false;
+    }
+
+    var now = Math.floor(Date.now() / 1000);
+
+    /* make sure the token does not have a timestamp in the future
+     * we'll allow the configured offset between servers */
+    if (this._timestamp !== 0 && this._timestamp - allowedOffset > now) {
+      err = new Error('Token:validate: token=' + this._unsignedToken +
+        ' : has future timestamp=' + this._timestamp +
+        ' : current time=' + now +
+        ' : allowed offset=' + allowedOffset);
+      winston.error(err);
+      return false;
+    }
+
+    /* make sure we don't have unlimited tokens unless we have
+     * explicitly enabled that option for our system. by default
+     * they should have an expiration date of less than 30 days */
+    if (this._expiryTime !== 0 || !ATHENZ_TOKEN_NO_EXPIRY || !allowNoExpiry) {
+      if (this._expiryTime < now) {
+        err = new Error('Token:validate: token=' + this._unsignedToken +
+          ' : has expired time=' + this._expiryTime +
+          ' : current time=' + now);
+        winston.error(err);
+        return false;
+      }
+      if (this._expiryTime > now + ATHENZ_TOKEN_MAX_EXPIRY + allowedOffset) {
+        err = new Error('Token:validate: token=' + this._unsignedToken +
+          ' : expires too far in the future=' + this._expiryTime +
+          ' : current time=' + now +
+          ' : max expiry=' + ATHENZ_TOKEN_MAX_EXPIRY +
+          ' : allowed offset=' + allowedOffset);
+        winston.error(err);
+        return false;
+      }
+    }
+
+    var verified = false; //fail safe
+    try {
+      verified = crypto.verify(this._unsignedToken, publicKey, this._signature, this._digestAlgorithm);
+      if (verified === false) {
+        err = new Error('Token:validate: token=' + this._unsignedToken +
+          ' : authentication failed');
+        winston.error(err);
+      }
+      winston.debug('validate: Token successfully authenticated');
+    } catch (e) {
+      winston.error('Token:validate: token=' + this._unsignedToken +
+        ' : verify signature failed due to Exception=' + e.message);
+    }
+
+    return verified;
+  }
+
+  getVersion() {
+    return this._version;
+  }
+
+  getSalt() {
+    return this._salt;
+  }
+
+  getHost() {
+    return this._host;
+  }
+
+  getDomain() {
+    return this._domain;
+  }
+
+  getSignature() {
+    return this._signature;
+  }
+
+  getTimestamp() {
+    return this._timestamp;
+  }
+
+  getExpiryTime() {
+    return this._expiryTime;
+  }
+
+  getSignedToken() {
+    return this._signedToken;
+  }
+
+  getKeyId() {
+    return this._keyId;
+  }
+
+  getIP() {
+    return this._ip;
+  }
+
+  getUnsignedToken() {
+    return this._unsignedToken;
+  }
+
+  /**
+   * Helper method to parse a credential to remove the signature from the
+   * raw credential string. Returning the unsigned credential.
+   * @param credential full token credentials including signature
+   * @return credentials without the signature
+   **/
+  static getUnsignedToken(credential) {
+    if (credential) {
+      var idx = credential.indexOf(';s=');
+      if (idx !== -1) {
+        credential = credential.substring(0, idx);
+      }
+    }
+
+    return credential;
+  }
+}
+
+module.exports = Token;
