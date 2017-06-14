a770ffbca27a4f3d11c0adc279f68718635d3429
Tatsuya Yano <tatyano@yahoo-corp.jp>
2017-06-14 18:51:10 +0900

    Extended config file

--- libs/nodejs/auth_core/config/config.js
+++ libs/nodejs/auth_core/config/config.js
@@ -14,21 +14,28 @@
 'use strict';
 
 var fs = require('fs');
-
 module.exports = function() {
   var defaultConfig = require('./default-config.js')();
   var extendedConfig = {};
+  var config = {};
   if (__dirname !== process.cwd() + '/config') {
+    var parentModule = module.parent;
+    while (parentModule.id.match(/node_modules\/auth_core\//)) {
+      parentModule = parentModule.parent;
+    }
+    var module_path = parentModule.id.match(/(.*)(node_modules\/(\w+))/g);
     try {
+      fs.statSync(module_path + '/config/config.js');
+      extendedConfig = require(module_path + '/config/config.js');
       fs.statSync(process.cwd() + '/config/config.js');
-      extendedConfig = require(process.cwd() + '/config/config.js')();
+      config = require(process.cwd() + '/config/config.js')();
     } catch (err) {
       if (err.code !== 'ENOENT') {
         console.error(err);
       }
     }
   }
-  var c = (extendedConfig.auth_core || {});
-
-  return Object.assign(defaultConfig, c);
+  var extendedConfigObj = (extendedConfig().auth_core || {});
+  var configObj = (config().auth_core || {});
+  return Object.assign(defaultConfig, extendedConfigObj, configObj);
 };
