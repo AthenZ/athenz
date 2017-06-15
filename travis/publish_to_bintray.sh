a48653f29fed2d940a1a39d8efe145811e2c50f1
Henry Avetisyan <hga@yahoo-inc.com>
2017-06-14 17:03:42 -0700

    [138] athenz use standard convention for artifact Ids and jar names

--- travis/publish_to_bintray.sh
+++ travis/publish_to_bintray.sh
@@ -3,15 +3,15 @@
 set -ev
 
 mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz --settings travis/settings-athenz-parent.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:zms_core --settings travis/settings-zms-core.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:zts_core --settings travis/settings-zts-core.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:auth_core --settings travis/settings-auth-core.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:zms_java_client --settings travis/settings-zms-java-client.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:zts_java_client --settings travis/settings-zts-java-client.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:zpe_java_client --settings travis/settings-zpe-java-client.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:client_common --settings travis/settings-client-common.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:server_common --settings travis/settings-server-common.xml
-mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:zpe_policy_updater --settings travis/settings-zpe-policy-updater.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms-core --settings travis/settings-zms-core.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-core --settings travis/settings-zts-core.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-auth-core --settings travis/settings-auth-core.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms-java-client --settings travis/settings-zms-java-client.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-java-client --settings travis/settings-zts-java-client.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpe-java-client --settings travis/settings-zpe-java-client.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-client-common --settings travis/settings-client-common.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-server-common --settings travis/settings-server-common.xml
+mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpe-policy-updater --settings travis/settings-zpe-policy-updater.xml
 mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms --settings travis/settings-athenz-zms.xml
 mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts --settings travis/settings-athenz-zts.xml
 mvn deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpu --settings travis/settings-athenz-zpu.xml
