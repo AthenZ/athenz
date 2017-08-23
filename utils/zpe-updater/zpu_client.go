da0e61db2a04230bcaab14fd8352791545769332
Ryuichi Okumura <okuryu@okuryu.com>
2017-08-24 00:38:10 +0900

    Remove duplicated license comment (#217)

--- utils/zpe-updater/zpu_client.go
+++ utils/zpe-updater/zpu_client.go
@@ -1,7 +1,5 @@
 // Copyright 2017 Yahoo Holdings, Inc.
 // Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
-// Copyright 2017 Yahoo Holdings, Inc.
-// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
 
 package zpu
 
