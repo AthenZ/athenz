f300f7411749f190c90b3c5845a13df529d4ac65
Henry Avetisyan <hga@verizonmedia.com>
2019-04-17 20:10:43 -0700

    new go client generator to handle content-type correctly; new zts-accesstoken utility

---
+++ utils/zts-accesstoken/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 Oath Holdings, Inc.
+// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
+
+// ZTS OAuth2 Access Token Client application in Go to request an access token from
+// ZTS Server for the given identity to access a role in a provider domain:
+package main
