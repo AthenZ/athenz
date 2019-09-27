57304cc882e228bfb5c045c9d013169249ba35be
Henry Avetisyan <hga@verizonmedia.com>
2019-09-27 09:53:07 -0700

    support parsing certs and extracting instance id in go

---
+++ libs/go/athenzutils/cert.go
@@ -0,0 +1,23 @@
+// Copyright 2019 Oath Holdings, Inc.
+// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
+
+package athenzutils
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+)
+
+func LoadX509Certificate(certFile string) (*x509.Certificate, error) {
+	pemBytes, err := ioutil.ReadFile(certFile)
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, fmt.Errorf("unable to parse x.509 certificate pem file: %s", certFile)
+	}
+	return x509.ParseCertificate(block.Bytes)
+}
---
+++ libs/go/athenzutils/instance.go
@@ -0,0 +1,39 @@
+// Copyright 2019 Oath Holdings, Inc.
+// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
+
+package athenzutils
+
+import (
+	"crypto/x509"
+	"fmt"
+	"strings"
+)
+
+func ExtractInstanceId(x509Cert x509.Certificate) (string, error) {
+
+	// first go through certificate values and see
+	// if we have an instance id specified in the uri
+	// format: athenz://instanceid/<provider>/<id>
+	if x509Cert.URIs != nil {
+		for _, uri := range x509Cert.URIs {
+			if uri.Scheme == "athenz" && uri.Host == "instanceid" {
+				comps := strings.Split(uri.Path, "/")
+				if len(comps) == 3 {
+					return comps[2], nil
+				}
+			}
+		}
+	}
+	// if we didn't find the instance id in the URI
+	// we're going to look for in the dnsName field
+	// format: <id>.instanceid.athenz.<provider-suffix>
+	if x509Cert.DNSNames != nil {
+		for _, dnsName := range x509Cert.DNSNames {
+			idx := strings.Index(dnsName, ".instanceid.athenz.")
+			if idx != -1 {
+				return dnsName[:idx], nil
+			}
+		}
+	}
+	return "", fmt.Errorf("unable to extract instance id from uri/dnsname fields")
+}
