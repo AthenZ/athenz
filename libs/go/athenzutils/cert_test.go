cde6ea06752e79ba27dc53450b2c9b675194a99f
Henry Avetisyan <havetisy@yahoo.com>
2019-09-27 12:28:50 -0700

    support parsing certs and extracting instance id in go (#779)

---
+++ libs/go/athenzutils/cert_test.go
@@ -0,0 +1,28 @@
+// Copyright 2019 Oath Holdings, Inc.
+// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
+
+package athenzutils
+
+import "testing"
+
+func TestLoadCertificate(test *testing.T) {
+
+	tests := []struct {
+		name     string
+		certFile string
+		valid    bool
+	}{
+		{"id1", "data/service_identity1.cert", true},
+		{"id2", "data/service_identity2.cert", true},
+		{"email", "data/valid_email_x509.cert", true},
+		{"notfoudn", "data/not_found.cert", false},
+	}
+	for _, tt := range tests {
+		test.Run(tt.name, func(t *testing.T) {
+			_, err := LoadX509Certificate(tt.certFile)
+			if tt.valid != (err == nil) {
+				test.Errorf("not expected response from processing test case: %s", tt.name)
+			}
+		})
+	}
+}
---
+++ libs/go/athenzutils/instance_test.go
@@ -0,0 +1,51 @@
+// Copyright 2019 Oath Holdings, Inc.
+// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
+
+package athenzutils
+
+import "testing"
+
+func TestExtractInstanceIdValid(test *testing.T) {
+
+	tests := []struct {
+		name       string
+		certFile   string
+		instanceId string
+	}{
+		{"nocn", "data/no_cn_x509.cert", "1001"},
+		{"invalidemail", "data/invalid_email_x509.cert", "1001"},
+		{"multiplemeail", "data/multiple_email_x509.cert", "1001"},
+		{"noemail", "data/no_email_x509.cert", "1001"},
+		{"uri", "data/athenz_instanceid_uri.cert", "id-001"},
+	}
+	for _, tt := range tests {
+		test.Run(tt.name, func(t *testing.T) {
+			x509Cert, _ := LoadX509Certificate(tt.certFile)
+			instanceId, _ := ExtractInstanceId(*x509Cert)
+			if instanceId != tt.instanceId {
+				test.Errorf("invalid instance id %s from %s", instanceId, tt.certFile)
+			}
+		})
+	}
+}
+
+func TestExtractInstanceIdInValid(test *testing.T) {
+
+	tests := []struct {
+		name      string
+		certFile  string
+	}{
+		{"id1", "data/service_identity1.cert"},
+		{"id2", "data/service_identity2.cert"},
+		{"email", "data/valid_email_x509.cert"},
+	}
+	for _, tt := range tests {
+		test.Run(tt.name, func(t *testing.T) {
+			x509Cert, _ := LoadX509Certificate(tt.certFile)
+			instanceId, err := ExtractInstanceId(*x509Cert)
+			if err == nil {
+				test.Errorf("no error from invalid file %s: %s", tt.certFile, instanceId)
+			}
+		})
+	}
+}
