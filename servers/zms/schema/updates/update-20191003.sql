c167d361442525d82728157eeac23d9a9c90b48d
Henry Avetisyan <hga@verizonmedia.com>
2019-10-03 10:05:59 -0700

    include mysql update for existing installations

---
+++ servers/zms/schema/updates/update-20191003.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `zms_server`.`pending_role_member` (
+  `role_id` INT UNSIGNED NOT NULL,
+  `principal_id` INT UNSIGNED NOT NULL,
+  `expiration` DATETIME(3) NULL,
+  `audit_ref` VARCHAR(512) NULL COMMENT 'Audit reference mandatory for membership changes of audit enabled / self serve roles',
+  `req_time` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`role_id`, `principal_id`),
+  INDEX `idx_principal` (`principal_id` ASC, `role_id` ASC) VISIBLE,
+  CONSTRAINT `fk_pending_role_member_role`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `zms_server`.`role` (`role_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pending_role_member_principal`
+    FOREIGN KEY (`principal_id`)
+    REFERENCES `zms_server`.`principal` (`principal_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
