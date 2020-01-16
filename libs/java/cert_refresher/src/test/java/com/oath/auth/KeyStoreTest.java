127ac4415ad3d652c95218d00328aee7ca214425
Sheridan C Rawlins <scr@verizonmedia.com>
2020-01-15 23:21:50 -0800

    Refactor so that cert and key pem representation can be passed in memory.

--- libs/java/cert_refresher/src/test/java/com/oath/auth/KeyStoreTest.java
+++ libs/java/cert_refresher/src/test/java/com/oath/auth/KeyStoreTest.java
@@ -20,24 +20,28 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.cert.X509Certificate;
 import java.util.Enumeration;
 
+import com.google.common.io.Resources;
 import org.junit.Test;
 
 public class KeyStoreTest {
 
     static final String ALIAS_NAME = "cn=athenz.syncer,o=my test company,l=sunnyvale,st=ca,c=us";
-    
+
     @Test
     public void testGetKeyStore() throws Exception {
 
         KeyStore keyStore = Utils.getKeyStore("truststore.jks", "123456".toCharArray());
         assertNotNull(keyStore);
-        
+
         // default password is secret - key exception
-        
+
         try {
             Utils.getKeyStore("truststore.jks");
             fail();
@@ -52,7 +56,7 @@ public class KeyStoreTest {
         assertNotNull(keyStore);
         String alias = null;
         for (Enumeration<?> e = keyStore.aliases(); e.hasMoreElements(); ) {
-            alias = (String)e.nextElement();
+            alias = (String) e.nextElement();
             assertEquals(ALIAS_NAME, alias);
         }
         X509Certificate[] chain = (X509Certificate[]) keyStore.getCertificateChain(alias);
@@ -66,16 +70,35 @@ public class KeyStoreTest {
         assertNotNull(keyStore);
         String alias = null;
         for (Enumeration<?> e = keyStore.aliases(); e.hasMoreElements(); ) {
-            alias = (String)e.nextElement();
+            alias = (String) e.nextElement();
             assertEquals(ALIAS_NAME, alias);
         }
-        
+
         X509Certificate[] chain = (X509Certificate[]) keyStore.getCertificateChain(alias);
         assertNotNull(chain);
         assertTrue(chain.length == 2);
     }
-    
-    @Test (expected = KeyRefresherException.class)
+
+    @Test
+    public void testCreateKeyStoreFromPems() throws Exception {
+        String athenzPublicCertPem = Resources.toString(
+                Resources.getResource("rsa_public_x510_w_intermediate.cert"), StandardCharsets.UTF_8);
+        String athenzPrivateKeyPem = Resources.toString(
+                Resources.getResource("rsa_private.key"), StandardCharsets.UTF_8);
+        KeyStore keyStore = Utils.createKeyStoreFromPems(athenzPublicCertPem, athenzPrivateKeyPem);
+        assertNotNull(keyStore);
+        String alias = null;
+        for (Enumeration<?> e = keyStore.aliases(); e.hasMoreElements(); ) {
+            alias = (String) e.nextElement();
+            assertEquals(ALIAS_NAME, alias);
+        }
+
+        X509Certificate[] chain = (X509Certificate[]) keyStore.getCertificateChain(alias);
+        assertNotNull(chain);
+        assertTrue(chain.length == 2);
+    }
+
+    @Test(expected = KeyRefresherException.class)
     public void testCreateKeyStoreEmpty() throws Exception {
         Utils.createKeyStore("rsa_public_x510_empty.cert", "rsa_private.key");
     }
