8edd307267908701d7d1ba91ba9396d7fc61c38b
Henry Avetisyan <hga@verizonmedia.com>
2020-04-18 16:48:41 -0700

    update jetty and other packages to their latest releases

--- servers/zms/src/test/java/com/yahoo/athenz/zms/ZMSBinderTest.java
+++ servers/zms/src/test/java/com/yahoo/athenz/zms/ZMSBinderTest.java
@@ -1,5 +1,6 @@
 package com.yahoo.athenz.zms;
 
+import com.yahoo.athenz.auth.impl.FilePrivateKeyStore;
 import org.testng.annotations.Test;
 
 public class ZMSBinderTest {
@@ -10,6 +11,7 @@ public class ZMSBinderTest {
         System.setProperty(ZMSConsts.ZMS_PROP_FILE_NAME, "src/test/resources/zms.properties");
         System.setProperty(ZMSConsts.ZMS_PROP_FILE_STORE_PATH, "/tmp/zms_core_unit_tests/");
         System.setProperty(ZMSConsts.ZMS_PROP_DOMAIN_ADMIN, "user.testadminuser");
+        System.setProperty(FilePrivateKeyStore.ATHENZ_PROP_PRIVATE_KEY, "src/test/resources/zms_private.pem");
 
         ZMSBinder binder = new ZMSBinder();
         binder.configure();
