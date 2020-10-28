fc2c81a7a28658a80e00e251d09252b1ce200ca0
Abhijeet V <abhijetv@verizonmedia.com>
2020-10-27 18:06:43 -0700

    removing mac specific instructions

--- start-local-athenz.sh
+++ start-local-athenz.sh
@@ -13,7 +13,7 @@ wait $MAKE_PID
 # convert pem cert to der format so that it can be imported into Mac login keychain
 openssl x509 -outform der -in sample/CAs/athenz_ca.pem -out sample/CAs/athenz_ca.der
 
-# change trust setting for the cert so that browser is happy
-security add-trusted-cert -k $HOME/Library/Keychains/login.keychain sample/CAs/athenz_ca.der
+# change trust setting for the cert so that browser is happy ( Mac specific instructions )
+# security add-trusted-cert -k $HOME/Library/Keychains/login.keychain sample/CAs/athenz_ca.der
 
 echo "You can access UI now at https://localhost"
--- stop-local-athenz.sh
+++ stop-local-athenz.sh
@@ -5,8 +5,8 @@ set -o pipefail
 
 cd docker
 
-# delete the cert from Mac keychain
-security delete-certificate -c "Sample Self Signed Athenz CA" -t
+# delete the cert from Mac keychain * Mac specific instructions )
+# security delete-certificate -c "Sample Self Signed Athenz CA" -t
 
 # stop containers and remove generated files
 make remove-local
