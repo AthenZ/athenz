ce2bf5da1350d082810137a500378496191519f2
Abhijeet V <abhijetv@verizonmedia.com>
2020-10-28 15:26:07 -0700

    minor refactoring

--- stop-local-athenz.sh
+++ stop-local-athenz.sh
@@ -5,10 +5,10 @@ set -o pipefail
 
 cd docker
 
-# delete the cert from Mac keychain * Mac specific instructions )
+# delete the cert from Mac keychain ( Mac specific optional step )
 # security delete-certificate -c "Sample Self Signed Athenz CA" -t
 
-# stop containers and remove generated files
+# stop containers and remove generated files, local images
 make remove-local
 
 cd ..
