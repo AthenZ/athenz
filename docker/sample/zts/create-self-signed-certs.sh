faff48dacffa921e9003e1b341cce95ca46d3fbf
Takayuki Ino <tino@yahoo-corp.jp>
2020-12-23 10:07:28 +0900

    Changed certificate expiration to 364 days

--- docker/sample/zts/create-self-signed-certs.sh
+++ docker/sample/zts/create-self-signed-certs.sh
@@ -17,7 +17,7 @@ CN='Sample Self Signed ZTS' SAN="${ZTS_HOST}" IPSAN="${HOST_EXTERNAL_IP}" openss
   -keyout "${DEV_ZTS_CERT_KEY_PATH}" \
   -out "${DEV_ZTS_CSR_PATH}" 2> /dev/null
 # sign request
-SAN="${ZTS_HOST}" IPSAN="${HOST_EXTERNAL_IP}" openssl x509 -req -days 3650 \
+SAN="${ZTS_HOST}" IPSAN="${HOST_EXTERNAL_IP}" openssl x509 -req -days 364 \
   -in "${DEV_ZTS_CSR_PATH}" \
   -CA "${DEV_ATHENZ_CA_PATH}" \
   -CAkey "${DEV_ATHENZ_CA_KEY_PATH}" \
