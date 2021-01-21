9370d50056e22215265906592ed3a165e882bcaa
dma <dma@verizonmedia.com>
2021-01-21 15:41:53 -0800

    update

--- screwdriver/scripts/install_deps.sh
+++ screwdriver/scripts/install_deps.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
-#set e
-#
-#apt-get update
-#
-##install nodejs 12.x repo
-#curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
-#apt-get install -y nodejs
-#apt-get install -y npm
-#apt-get install -y software-properties-common
-#add-apt-repository -y ppa:ubuntu-toolchain-r/test
-#apt-get install -y gcc
-#apt-get install -y g++
-#npm install -g npm@latest
+set e
+
+apt-get update
+
+#install nodejs 12.x repo
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+apt-get install -y nodejs
+apt-get install -y npm
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get install -y gcc
+apt-get install -y g++
+npm install -g npm@latest
 
 # install go
 
 # check all installed dependencies
-echo "Java Version: "
+echo "-----------------Java Version: -----------------"
 java -version
-
-echo
+echo "-----------------Maven Version: -----------------"
 mvn -version
+echo "-----------------Nodejs Version: -----------------"
 node -v
+echo "-----------------NPM Version: -----------------"
 npm -v
+echo "-----------------Golang Version: -----------------"
 go version
