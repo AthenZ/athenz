1a6d6ee7eddfbfb5b88cbcc96b084a31c9a1274b
dma <dma@verizonmedia.com>
2021-01-21 15:38:18 -0800

    update

--- screwdriver/scripts/install_deps.sh
+++ screwdriver/scripts/install_deps.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-set e
-
-apt-get update
-
-apt-get install -y nodejs
-apt-get install -y npm
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get install -y gcc
-apt-get install -y g++
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.37.2/install.sh | bash
-
-#nvm install 12
-npm install -g npm@latest
-
-
+#set e
+#
+#apt-get update
+#
+##install nodejs 12.x repo
+#curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+#apt-get install -y nodejs
+#apt-get install -y npm
+#apt-get install -y software-properties-common
+#add-apt-repository -y ppa:ubuntu-toolchain-r/test
+#apt-get install -y gcc
+#apt-get install -y g++
+#npm install -g npm@latest
 
 # install go
 
 # check all installed dependencies
+echo "Java Version: "
 java -version
+
+echo
 mvn -version
 node -v
 npm -v
