5bb7ac4600bcb79de5c712c346c0bac8f4c6b9a8
dma <dma@verizonmedia.com>
2021-01-21 16:29:21 -0800

    update

--- screwdriver/scripts/install_deps.sh
+++ screwdriver/scripts/install_deps.sh
@@ -12,6 +12,7 @@ apt-get install -y software-properties-common
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get install -y gcc
 apt-get install -y g++
+apt-get install build-essential
 npm install -g npm@latest
 
 # install go
