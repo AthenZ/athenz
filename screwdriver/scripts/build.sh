e1ad63543c0cfc47d4abeb94dbe9c852a42ba032
dma <dma@verizonmedia.com>
2021-01-26 13:03:22 -0800

    update

--- screwdriver/scripts/build.sh
+++ screwdriver/scripts/build.sh
@@ -5,6 +5,5 @@ set -ev
 readonly NUM_THREADS=$(( $(nproc) + 2 ))
 
 export MAVEN_OPTS="-Xss1m -Xms128m -Xmx2g"
-export ATHENZ_MAVEN_EXTRA_OPTS="${ATHENZ_MAVEN_EXTRA_OPTS:+${ATHENZ_MAVEN_EXTRA_OPTS} }--no-snapshot-updates --batch-mode --threads ${NUM_THREADS}"
 
-mvn install
+mvn install --no-snapshot-updates --batch-mode --threads ${NUM_THREADS}
