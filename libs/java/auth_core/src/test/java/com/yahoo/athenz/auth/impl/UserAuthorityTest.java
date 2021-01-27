a92cf161bdec2b2a775db0b5eca6486408e8f1cc
Henry Avetisyan <hga@verizonmedia.com>
2021-01-27 13:32:19 -0800

    use mock class to avoid jvm crash with some systems

--- libs/java/auth_core/src/test/java/com/yahoo/athenz/auth/impl/UserAuthorityTest.java
+++ libs/java/auth_core/src/test/java/com/yahoo/athenz/auth/impl/UserAuthorityTest.java
@@ -35,7 +35,7 @@ public class UserAuthorityTest {
     @Test
     public void testUserAuthority() throws PAMException {
         PAM pam = Mockito.mock(PAM.class);
-        UnixUser user = new UnixUser(System.getenv("USER"));
+        UnixUser user = Mockito.mock(UnixUser.class);
         Mockito.when(pam.authenticate("testuser", "testpwd")).thenReturn(user);
         UserAuthority userAuthority = new UserAuthority();
         userAuthority.setPAM(pam);
@@ -95,7 +95,7 @@ public class UserAuthorityTest {
     @Test
     public void testNullSimplePrincipal() throws PAMException {
         PAM pam = Mockito.mock(PAM.class);
-        UnixUser user = new UnixUser(System.getenv("USER"));
+        UnixUser user = Mockito.mock(UnixUser.class);
         Mockito.when(pam.authenticate("testuser", "testpwd")).thenReturn(user);
         UserAuthority authority = new UserAuthority();
         UserAuthority userAuthority = Mockito.spy(authority);
