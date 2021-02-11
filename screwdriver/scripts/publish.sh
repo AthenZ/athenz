6a2378bd1c1ecdafbd37469cfee6b0f0a1050c27
Henry Avetisyan <hga@verizonmedia.com>
2021-02-10 17:17:49 -0800

    move publish from bintray to github packages

--- screwdriver/scripts/publish.sh
+++ screwdriver/scripts/publish.sh
@@ -2,22 +2,18 @@
 
 set -ev
 
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz --settings screwdriver/settings/settings-athenz-parent.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms-core --settings screwdriver/settings/settings-zms-core.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-core --settings screwdriver/settings/settings-zts-core.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-auth-core --settings screwdriver/settings/settings-auth-core.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms-java-client --settings screwdriver/settings/settings-zms-java-client.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-java-client-core --settings screwdriver/settings/settings-zts-java-client-core.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-java-client --settings screwdriver/settings/settings-zts-java-client.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpe-java-client --settings screwdriver/settings/settings-zpe-java-client.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-client-common --settings screwdriver/settings/settings-client-common.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-cert-refresher --settings screwdriver/settings/settings-cert-refresher.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-server-common --settings screwdriver/settings/settings-server-common.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-instance-provider --settings screwdriver/settings/settings-athenz-instance-provider.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-jetty-container --settings screwdriver/settings/settings-athenz-jetty-container.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpe-policy-updater --settings screwdriver/settings/settings-zpe-policy-updater.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms --settings screwdriver/settings/settings-athenz-zms.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts --settings screwdriver/settings/settings-athenz-zts.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpu --settings screwdriver/settings/settings-athenz-zpu.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-ui --settings screwdriver/settings/settings-athenz-ui.xml
-mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-utils --settings screwdriver/settings/settings-athenz-utils.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms-core --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-core --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-auth-core --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms-java-client --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-java-client-core --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-java-client --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpe-java-client --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-client-common --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-cert-refresher --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-server-common --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-instance-provider --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-jetty-container --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zpu --settings screwdriver/settings/settings-publish.xml
+mvn -B deploy -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-utils --settings screwdriver/settings/settings-publish.xml
