94f387011b6990d3a79d7a5134b9f01663be740d
Henry Avetisyan <hga@verizonmedia.com>
2021-02-25 12:38:46 -0800

    enable auto-deploy from SD
    Signed-off-by: Henry Avetisyan <hga@verizonmedia.com>

--- screwdriver/scripts/publish.sh
+++ screwdriver/scripts/publish.sh
@@ -2,6 +2,9 @@
 
 set -ev
 
+openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in deploy/pubring.gpg.enc -out deploy/pubring.gpg -d
+openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in deploy/secring.gpg.enc -out deploy/secring.gpg -d
+
 mvn -B deploy -P ossrh -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz --settings screwdriver/settings/settings-publish.xml
 mvn -B deploy -P ossrh -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zms-core --settings screwdriver/settings/settings-publish.xml
 mvn -B deploy -P ossrh -Dmaven.test.skip=true --projects com.yahoo.athenz:athenz-zts-core --settings screwdriver/settings/settings-publish.xml
