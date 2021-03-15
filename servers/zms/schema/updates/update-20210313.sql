03cdb29066375eed3376a517096a87fe1ac5a643
Henry Avetisyan <havetisy@yahoo.com>
2021-03-15 12:10:35 -0700

    fix group db name in db update script (#1354)
    Signed-off-by: Henry Avetisyan <hga@verizonmedia.com>
    
    Co-authored-by: Henry Avetisyan <hga@verizonmedia.com>

--- servers/zms/schema/updates/update-20210313.sql
+++ servers/zms/schema/updates/update-20210313.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `zms_server`.`domain` ADD `business_service` VARCHAR(256) NOT NULL DEFAULT '';
 CREATE INDEX `idx_business_service` ON `zms_server`.`domain` (`business_service` ASC);
-ALTER TABLE `zms_server`.`group` ADD `member_expiry_days` INT NOT NULL DEFAULT 0;
-ALTER TABLE `zms_server`.`group` ADD `service_expiry_days` INT NOT NULL DEFAULT 0;
+ALTER TABLE `zms_server`.`principal_group` ADD `member_expiry_days` INT NOT NULL DEFAULT 0;
+ALTER TABLE `zms_server`.`principal_group` ADD `service_expiry_days` INT NOT NULL DEFAULT 0;
