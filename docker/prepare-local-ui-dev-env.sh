f45cf1622ff4e65e9ae24fb7087a9a9f79d15412
Abhijeet V <31417623+abvaidya@users.noreply.github.com>
2021-03-17 07:58:59 -0700

    local ui development prep (#1360)

---
+++ docker/prepare-local-ui-dev-env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+BASE_DIR=$(git rev-parse --show-toplevel)
+cp ${BASE_DIR}/docker/ui/var/keys/*.pem ${BASE_DIR}/ui/keys
+cp ${BASE_DIR}/docker/ui/var/keys/cookie-session ${BASE_DIR}/ui/keys
+
+cp ${BASE_DIR}/docker/ui/conf/users_data.json ${BASE_DIR}/ui/src/config
+cp ${BASE_DIR}/docker/ui/conf/athenz.conf  ${BASE_DIR}/ui/src/config
+
+printf '\n'
+printf 'If you are on Mac you can run the following command to add the self-signed certificate to login keychain, so that browser does not complain about it. \n'
+printf "security add-trusted-cert -k %s/Library/Keychains/login.keychain ${BASE_DIR}/docker/sample/CAs/athenz_ca.der\n" "$HOME"
+printf '\n'
+
+exit 0
