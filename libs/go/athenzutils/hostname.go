25497d28b7bac5bfe382f131e1d6498a8806289c
Dmitry Savintsev <dmitris@users.noreply.github.com>
2021-06-09 22:41:16 +0200

    go/athenzutils: add ExtractHostname function (#1511)
    * go/athenzutils: add ExtractHostname function
    
    Signed-off-by: Dmitry Savintsev <dsavints@verizonmedia.com>
    
    * copy locally the test certificate with hostname URI
    
    Signed-off-by: Dmitry Savintsev <dsavints@verizonmedia.com>

---
+++ libs/go/athenzutils/hostname.go
@@ -0,0 +1,28 @@
+// Copyright Athenz Authors
+// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
+
+package athenzutils
+
+import (
+	"crypto/x509"
+)
+
+// ExtractHostname returns the hostname from the SAN URIs field
+// of the given certificate:
+// https://golang.org/pkg/crypto/x509/#Certificate.URIs.
+// If the certificate does not have the hostname in the SAN URIs field,
+// an empty string is returned.
+func ExtractHostname(x509Cert x509.Certificate) string {
+
+	// go through the certificate SAN URIs and see
+	// if we have an instance id specified in the uri
+	// format: athenz://hostname/<hostname>
+	for _, uri := range x509Cert.URIs {
+		if uri.Scheme == "athenz" && uri.Host == "hostname" {
+			if len(uri.Path) > 1 {
+				return uri.Path[1:]
+			}
+		}
+	}
+	return ""
+}
---
+++ libs/go/athenzutils/hostname_test.go
@@ -0,0 +1,36 @@
+// Copyright Athenz Authors
+// Licensed under the terms of the Apache version 2.0 license. See LICENSE file for terms.
+
+package athenzutils
+
+import "testing"
+
+func TestExtractHostname(test *testing.T) {
+
+	tests := []struct {
+		name     string
+		certFile string
+		hostname string
+	}{
+		{"nocn", "data/no_cn_x509.cert", ""},
+		{"emailnouri", "data/valid_email_x509.cert", ""},
+		{
+			"urihostname",
+			"data/athenz.examples.uri-hostname-only.pem",
+			"abc.athenz.com",
+		},
+	}
+	for _, tt := range tests {
+		test.Run(tt.name, func(t *testing.T) {
+			x509Cert, err := LoadX509Certificate(tt.certFile)
+			if err != nil {
+				t.Fatal(err)
+			}
+			hostname := ExtractHostname(*x509Cert)
+			if hostname != tt.hostname {
+				test.Errorf("invalid hostname '%s' from %s, want %s",
+					hostname, tt.certFile, tt.hostname)
+			}
+		})
+	}
+}
