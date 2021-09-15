bc3af0e38f94679e60632843600e0257a48d5226
dvirguttman <dvirguttman@gmail.com>
2021-09-15 17:39:45 +0300

    Domain changes messaging skeleton (#1619)
    * Domain changes messaging skeleton
    
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>

---
+++ libs/java/client_common/src/test/java/com/yahoo/athenz/common/messaging/DomainChangeMessageTest.java
@@ -0,0 +1,64 @@
+/*
+ *
+ *  * Copyright The Athenz Authors
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.yahoo.athenz.common.messaging;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class DomainChangeMessageTest {
+
+    @Test
+    public void testDomainChange() {
+        
+        DomainChangeMessage domainChange = new DomainChangeMessage();
+        domainChange.setDomainName("domain")
+            .setPublished(123L)
+            .setUuid("uuid")
+            .setObjectName("role-obj")
+            .setObjectType(DomainChangeMessage.ObjectType.ROLE)
+            .setApiName("putRole");
+        
+        assertEquals(domainChange.getDomainName(), "domain");
+        assertEquals(domainChange.getPublished(), 123L);
+        assertEquals(domainChange.getUuid(), "uuid");
+        assertEquals(domainChange.getObjectType(), DomainChangeMessage.ObjectType.ROLE);
+        assertEquals(domainChange.getObjectName(), "role-obj");
+        assertEquals(domainChange.getApiName(), "putRole");
+        assertEquals(domainChange, domainChange);
+        
+        assertFalse(domainChange.equals(null));
+        assertFalse(domainChange.equals("nonDomainChange"));
+
+        DomainChangeMessage domainChange1 = new DomainChangeMessage();
+        domainChange1.setDomainName("domain")
+            .setPublished(123L)
+            .setUuid("uuid")
+            .setObjectName("role-obj")
+            .setObjectType(DomainChangeMessage.ObjectType.ROLE)
+            .setApiName("putRole");
+        
+        assertEquals(domainChange, domainChange1);
+        assertEquals(domainChange.hashCode(), domainChange1.hashCode());
+
+        domainChange1.setObjectType(DomainChangeMessage.ObjectType.POLICY);
+        assertNotEquals(domainChange, domainChange1);
+    }
+    
+}
---
+++ libs/java/server_common/src/test/java/com/yahoo/athenz/common/messaging/NoOpDomainChangeMessagePublisherTest.java
@@ -0,0 +1,61 @@
+/*
+ *
+ *  * Copyright The Athenz Authors
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.yahoo.athenz.common.messaging;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static com.yahoo.athenz.common.messaging.DomainChangePublisherFactory.ZMS_PROP_DOMAIN_CHANGE_PUBLISHER_CLASS;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+public class NoOpDomainChangeMessagePublisherTest {
+
+    @BeforeClass
+    public void setUp() {
+        System.clearProperty(ZMS_PROP_DOMAIN_CHANGE_PUBLISHER_CLASS);
+    }
+
+    @Test
+    public void invalidFactoryClass() {
+        System.setProperty(ZMS_PROP_DOMAIN_CHANGE_PUBLISHER_CLASS, "com.yahoo.athenz.zms.messaging.noexist");
+        try {
+            DomainChangePublisherFactory.create();
+            fail();
+        } catch (ExceptionInInitializerError ignored) { }
+        System.clearProperty(ZMS_PROP_DOMAIN_CHANGE_PUBLISHER_CLASS);
+    }
+    
+    @Test
+    public void testNoOpDomainChangePublisher() {
+        DomainChangePublisher noOpPublisher = DomainChangePublisherFactory.create();
+        assertTrue(noOpPublisher instanceof NoOpDomainChangePublisher);
+        DomainChangeMessage domainChangeMessage = new DomainChangeMessage();
+        domainChangeMessage.setDomainName("someDomain")
+            .setPublished(Instant.now().toEpochMilli())
+            .setUuid(UUID.randomUUID().toString())
+            .setObjectName("group-obj")
+            .setObjectType(DomainChangeMessage.ObjectType.POLICY)
+            .setApiName("putGroup");
+        noOpPublisher.publishMessage(domainChangeMessage);
+    }
+}
