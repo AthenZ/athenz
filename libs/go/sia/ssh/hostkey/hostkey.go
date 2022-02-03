79b4d501ad87b491d00785378462281417c69e50
Sasidhar <psasidhar@gmail.com>
2022-02-03 08:05:11 -0800

    Adding methods to use ssh host keys and certs (#1766)
    Signed-off-by: Sasidhar Palaka <palakas@verizonmedia.com>
    
    Co-authored-by: Sasidhar Palaka <palakas@verizonmedia.com>

---
+++ libs/go/sia/ssh/hostkey/hostkey.go
@@ -0,0 +1,98 @@
+//
+// Copyright The Athenz Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package hostkey
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"time"
+)
+
+type KeyType int
+
+const (
+	_ KeyType = iota
+	Rsa
+	Ecdsa
+	Ed25519
+)
+
+const DEFAULT_KEYTYPE = "rsa"
+
+var toString = map[KeyType]string{
+	Rsa:     "rsa",
+	Ecdsa:   "ecdsa",
+	Ed25519: "ed25519",
+}
+
+var toId = map[string]KeyType{
+	"rsa":     Rsa,
+	"ecdsa":   Ecdsa,
+	"ed25519": Ed25519,
+}
+
+// MarshalJSON marshals the enum as a quoted json string
+func (s KeyType) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString(`"`)
+	buffer.WriteString(toString[s])
+	buffer.WriteString(`"`)
+	return buffer.Bytes(), nil
+}
+
+// UnmarshalJSON unmashals a quoted json string to the enum value
+func (s *KeyType) UnmarshalJSON(b []byte) error {
+	var j string
+	err := json.Unmarshal(b, &j)
+	if err != nil {
+		return err
+	}
+
+	if val, ok := toId[j]; ok {
+		*s = val
+	} else {
+		*s = toId[DEFAULT_KEYTYPE]
+	}
+	return nil
+}
+
+// PubKey reads the content of file, until a non-nil error and upto n times
+// each retry sleeps for a second
+func PubKey(file string, n int) ([]byte, error) {
+	var pubKey []byte
+	var err error
+	for i := 0; i < n; i++ {
+		pubKey, err = ioutil.ReadFile(file)
+		if err == nil {
+			return pubKey, nil
+		}
+		time.Sleep(time.Second)
+	}
+	return nil, err
+}
+
+// PubKeyFile returns the path to the public key of the host key type passed in
+func PubKeyFile(sshDir string, keyType KeyType) string {
+	return filepath.Join(sshDir, fmt.Sprintf("ssh_host_%s_key.pub", toString[keyType]))
+}
+
+// PubCertFile returns the path to the ssh host certificate based on the keyType
+func CertFile(sshDir string, keyType KeyType) string {
+	return filepath.Join(sshDir, fmt.Sprintf("ssh_host_%s_key-cert.pub", toString[keyType]))
+}
---
+++ libs/go/sia/ssh/hostkey/hostkey_test.go
@@ -0,0 +1,134 @@
+//
+// Copyright The Athenz Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package hostkey
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPubKey(t *testing.T) {
+	tmpSshDir, e := ioutil.TempDir("", "ssh")
+	require.Nilf(t, e, "unexpected error: %v", e)
+	defer os.RemoveAll(tmpSshDir)
+
+	sshPubFile := fmt.Sprintf(filepath.Join(tmpSshDir, "ssh_host_ed25519_key.pub"))
+
+	var pubKey []byte
+	var err error
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		pubKey, err = PubKey(sshPubFile, 8)
+	}()
+
+	// when there is no file, PubKey will attempt to retry
+	time.Sleep(1 * time.Second)
+	assert.Nil(t, pubKey)
+
+	// let's create a file now for testing
+	ioutil.WriteFile(sshPubFile, []byte("test key"), 0400)
+
+	// wait for PubKey to finish
+	wg.Wait()
+
+	// let's give it an extra second
+	time.Sleep(1 * time.Second)
+
+	// error should be nil now
+	assert.Nil(t, err)
+	assert.True(t, string(pubKey) == "test key")
+}
+
+func TestKeyType_UnmarshalJSON(t *testing.T) {
+	sample := `{ "ssh_host_key_type": "rsa" }`
+
+	type config struct {
+		SshHostKeyType KeyType `json:"ssh_host_key_type,omitempty"`
+	}
+
+	data := config{}
+
+	err := json.Unmarshal([]byte(sample), &data)
+	require.Nilf(t, err, "unexpected err: %v", err)
+	assert.Equalf(t, Rsa, data.SshHostKeyType, "unexpected data: %+v", data)
+}
+
+func TestKeyType_UnmarshalJSON_Unknown(t *testing.T) {
+	sample := `{ "ssh_host_key_type": "unknown" }`
+
+	type config struct {
+		SshHostKeyType KeyType `json:"ssh_host_key_type,omitempty"`
+	}
+
+	data := config{}
+
+	err := json.Unmarshal([]byte(sample), &data)
+	require.Nilf(t, err, "unexpected err: %v", err)
+	assert.Equalf(t, Rsa, data.SshHostKeyType, "unexpected data: %+v", data)
+}
+
+func TestKeyType_UnmarshalJSON_Empty(t *testing.T) {
+	type config struct {
+		SshHostKeyType KeyType `json:"ssh_host_key_type,omitempty"`
+	}
+
+	data := config{}
+
+	err := json.Unmarshal([]byte(`{}`), &data)
+	require.Nilf(t, err, "unexpected err: %v", err)
+	assert.Equalf(t, KeyType(0), data.SshHostKeyType, "unexpected data: %+v", data)
+}
+
+func TestKeyType_MarshalJSON(t *testing.T) {
+	type config struct {
+		SshHostKeyType KeyType `json:"ssh_host_key_type,omitempty"`
+	}
+
+	data := config{
+		SshHostKeyType: Ecdsa,
+	}
+
+	expectedStr := `{"ssh_host_key_type":"ecdsa"}`
+
+	s, err := json.Marshal(data)
+	require.Nilf(t, err, "unexpected err: %v", err)
+	assert.Equalf(t, []byte(expectedStr), s, "unexpected data: %q", s)
+}
+
+func TestPubKeyFile(t *testing.T) {
+	for kt, s := range toString {
+		assert.Equal(t, fmt.Sprintf("/tmp/ssh_host_%s_key.pub", s), PubKeyFile("/tmp", kt))
+	}
+}
+
+func TestCertKeyFile(t *testing.T) {
+	for kt, s := range toString {
+		assert.Equal(t, fmt.Sprintf("/tmp/ssh_host_%s_key-cert.pub", s), CertFile("/tmp", kt))
+	}
+}
