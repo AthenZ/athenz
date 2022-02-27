409a6f13b534306598f2f65aeab9cefbd3a042d2
Henry Avetisyan <havetisy@yahoo.com>
2022-02-27 12:08:38 -0800

    Athenz ZTS QosFilter support for cert requests (#1797)

---
+++ libs/java/server_common/src/main/java/com/yahoo/athenz/common/filter/impl/AthenzZTSQoSFilter.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright Athenz Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yahoo.athenz.common.filter.impl;
+
+import org.eclipse.jetty.util.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * AthenzZTSQoSFilter extends Jetty's QoSFilter to allow separate
+ * handling of URIs for certificates and tokens
+ *
+ * {@literal
+ * <filter>
+ *   <filter-name>AthenzZTSQoSFilterCerts</filter-name>
+ *   <filter-class>com.yahoo.athenz.common.filter.impl.AthenzZTSQoSFilter</filter-class>
+ *   <init-param>
+ *     <param-name>certRequests</param-name>
+ *     <param-value>true</param-value>
+ *   </init-param>
+ *   <init-param>
+ *     <param-name>maxRequests</param-name>
+ *     <param-value>5</param-value>
+ *   </init-param>
+ *   </filter>
+ *   <filter-mapping>
+ *     <filter-name>AthenzZTSQoSFilterCerts</filter-name>
+ *     <url-pattern>/*</url-pattern>
+ *   </filter-mapping>
+ * </filter>
+ * }
+ */
+
+public class AthenzZTSQoSFilter extends org.eclipse.jetty.servlets.QoSFilter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AthenzZTSQoSFilter.class);
+
+    static final String CERT_REQUESTS_INIT_PARAM = "certRequests";
+    static final String HTTP_POST = "POST";
+
+    private boolean certRequestConfig = false;
+    
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+        final String certRequestsValue = filterConfig.getInitParameter(CERT_REQUESTS_INIT_PARAM);
+        if (!StringUtil.isEmpty(certRequestsValue)) {
+            certRequestConfig = Boolean.parseBoolean(certRequestsValue);
+        }
+
+        super.init(filterConfig);
+    }
+    
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+
+        boolean certRequest = isCertRequest(servletRequest);
+        if ((certRequestConfig && certRequest) || (!certRequestConfig && !certRequest)) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("calling QoSFilter for processing cert-config({})/cert-request({})",
+                        certRequestConfig, certRequest);
+            }
+            super.doFilter(servletRequest, servletResponse, chain);
+        } else {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("skipping to next filter in chain cert-config({})/cert-request({})",
+                        certRequestConfig, certRequest);
+            }
+            chain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    public boolean getCertRequestConfig() {
+        return certRequestConfig;
+    }
+
+    boolean isCertRequest(ServletRequest servletRequest) {
+
+        // our host and ssh certificate request URIs are the following POST paths:
+        //
+        //  @Path("/instance/{provider}/{domain}/{service}/{instanceId}")
+        //  @Path("/instance/{domain}/{service}/refresh")
+        //  @Path("/instance")
+        //  @Path("/domain/{domainName}/role/{roleName}/token") (deprecated rolecert api)
+        //  @Path("/rolecert")
+        //  @Path("/sshcert")
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("processing request: {} {}", request.getMethod(), request.getRequestURI());
+        }
+
+        // so the first quick check is if the request is not a POST
+        // request, then it cannot be a cert request
+
+        if (!HTTP_POST.equalsIgnoreCase(request.getMethod())) {
+            return false;
+        }
+
+        final String uri = request.getRequestURI();
+        return uri.startsWith("/zts/v1/instance") ||
+                uri.startsWith("/zts/v1/rolecert") ||
+                uri.startsWith("/zts/v1/sshcert") ||
+                (uri.startsWith("/zts/v1/domain/") && uri.endsWith("/token"));
+    }
+}
---
+++ libs/java/server_common/src/test/java/com/yahoo/athenz/common/filter/impl/AthenzZTSQoSFilterTest.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright Athenz Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yahoo.athenz.common.filter.impl;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mockito.Mockito;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class AthenzZTSQoSFilterTest {
+
+    private class QoSFilterChain implements FilterChain {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse servletResponse) {
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            response.setStatus(200);
+        }
+    }
+
+    @Test
+    public void testAthenzQoSFilterInitCerts() {
+
+        final String maxRequest = "100";
+
+        AthenzZTSQoSFilter filter = new AthenzZTSQoSFilter();
+        assertNotNull(filter);
+
+        FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+        Mockito.when(filterConfig.getInitParameter("maxRequests")).thenReturn(maxRequest);
+        Mockito.when(filterConfig.getInitParameter("certRequests")).thenReturn("true");
+        filter.init(filterConfig);
+        assertTrue(filter.getCertRequestConfig());
+
+        MockHttpServletRequest request =  new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setRequestURI("/zts/v1/instance");
+
+        MockHttpServletResponse response =  new MockHttpServletResponse();
+        QoSFilterChain chain = new QoSFilterChain();
+
+        try {
+            filter.doFilter(request, response, chain);
+        } catch (IOException | ServletException e) {
+            fail();
+        }
+        assertEquals(response.getStatus(), 200);
+        assertEquals(filter.getMaxRequests(), Integer.valueOf(maxRequest).intValue());
+    }
+
+    @Test
+    public void testAthenzQoSFilterInitNonCerts() {
+
+        final String maxRequest = "100";
+
+        AthenzZTSQoSFilter filter = new AthenzZTSQoSFilter();
+        assertNotNull(filter);
+
+        FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+        Mockito.when(filterConfig.getInitParameter("maxRequests")).thenReturn(maxRequest);
+        Mockito.when(filterConfig.getInitParameter("certRequests")).thenReturn("false");
+        filter.init(filterConfig);
+        assertFalse(filter.getCertRequestConfig());
+
+        MockHttpServletRequest request =  new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setRequestURI("/zts/v1/oauth2/token");
+
+        MockHttpServletResponse response =  new MockHttpServletResponse();
+        QoSFilterChain chain = new QoSFilterChain();
+
+        try {
+            filter.doFilter(request, response, chain);
+        } catch (IOException | ServletException e) {
+            fail();
+        }
+        assertEquals(response.getStatus(), 200);
+        assertEquals(filter.getMaxRequests(), Integer.valueOf(maxRequest).intValue());
+    }
+
+    @Test
+    public void testAthenzQoSFilterInitCertRequestNonCertConfig() {
+
+        final String maxRequest = "100";
+
+        AthenzZTSQoSFilter filter = new AthenzZTSQoSFilter();
+        assertNotNull(filter);
+
+        FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+        Mockito.when(filterConfig.getInitParameter("maxRequests")).thenReturn(maxRequest);
+        Mockito.when(filterConfig.getInitParameter("certRequests")).thenReturn("false");
+        filter.init(filterConfig);
+        assertFalse(filter.getCertRequestConfig());
+
+        MockHttpServletRequest request =  new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setRequestURI("/zts/v1/instance");
+
+        MockHttpServletResponse response =  new MockHttpServletResponse();
+        QoSFilterChain chain = new QoSFilterChain();
+
+        try {
+            filter.doFilter(request, response, chain);
+        } catch (IOException | ServletException e) {
+            fail();
+        }
+        assertEquals(response.getStatus(), 200);
+        assertEquals(filter.getMaxRequests(), Integer.valueOf(maxRequest).intValue());
+    }
+
+    @Test
+    public void testAthenzQoSFilterInitNoAttr() {
+
+        final String maxRequest = "100";
+
+        AthenzZTSQoSFilter filter = new AthenzZTSQoSFilter();
+        assertNotNull(filter);
+
+        FilterConfig filterConfig = Mockito.mock(FilterConfig.class);
+        Mockito.when(filterConfig.getInitParameter("maxRequests")).thenReturn(maxRequest);
+        filter.init(filterConfig);
+        assertFalse(filter.getCertRequestConfig());
+    }
+
+    @DataProvider
+    public Object[][] httpRequests() {
+
+        //  @Path("/instance/{provider}/{domain}/{service}/{instanceId}")
+        //  @Path("/instance/{domain}/{service}/refresh")
+        //  @Path("/instance")
+        //  @Path("/domain/{domainName}/role/{roleName}/token") (deprecated rolecert api)
+        //  @Path("/rolecert")
+        //  @Path("/sshcert")
+
+        return new Object[][]{
+                {"GET", "/zts/v1/token", false},
+                {"POST", "/zts/v1/instance", true},
+                {"POST", "/zts/v1/instance/aws/athenz/api/001", true},
+                {"POST", "/zts/v1/instance/athenz/api/refresh", true},
+                {"POST", "/zts/v1/instance/something/new", true},
+                {"GET", "/zts/v1/instance", false},
+                {"GET", "/zts/v1/instance/aws/athenz/api/001", false},
+                {"POST", "/zts/v1/rolecert", true},
+                {"POST", "/zts/v1/rolecert/new", true},
+                {"GET", "/zts/v1/rolecert", false},
+                {"POST", "/zts/v1/sshcert", true},
+                {"POST", "/zts/v1/sshcert/new", true},
+                {"POST", "/zts/v1/newcert", false},
+                {"GET", "/zts/v1/sshcert", false},
+                {"POST", "/zts/v1/domain/athenz/role/admin/token", true},
+                {"POST", "/zts/v1/domain/athenz/role/admin/creds", false},
+                {"POST", "/zts/v1/access/athenz/role/admin/token", false},
+                {"GET", "/zts/v1/domain/athenz/role/admin/token", false},
+        };
+    }
+
+    @Test(dataProvider = "httpRequests")
+    public void testIsCertRequest(final String method, final String uri, boolean certRequest) {
+
+        MockHttpServletRequest request =  new MockHttpServletRequest();
+        request.setMethod(method);
+        request.setRequestURI(uri);
+
+        AthenzZTSQoSFilter filter = new AthenzZTSQoSFilter();
+        assertEquals(filter.isCertRequest(request), certRequest);
+    }
+}
