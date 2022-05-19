b5ae7646b9892167c60b1942b2da9fc06cb3f124
Sasidhar <psasidhar@gmail.com>
2022-05-19 09:49:30 -0700

    Adding an api method to MsdStoreConnection to help in building caches (#1870)
    Signed-off-by: Sasidhar Palaka <palakas@verizonmedia.com>
    
    Co-authored-by: Sasidhar Palaka <palakas@verizonmedia.com>

---
+++ libs/java/server_common/src/test/java/com/yahoo/athenz/common/server/msd/net/InetAddressMapTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright The Athenz Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yahoo.athenz.common.server.msd.net;
+
+import com.yahoo.athenz.msd.DynamicWorkload;
+import com.yahoo.athenz.msd.StaticWorkload;
+import org.testng.annotations.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+import static org.testng.Assert.*;
+
+public class InetAddressMapTest {
+
+    @Test
+    public void putIp() {
+        InetAddressMap<DynamicWorkload> dynamicIps = new InetAddressMap<>(new TreeMap<>(InetComparator::compare), new TreeMap<>(InetComparator::compare));
+
+        dynamicIps.putIp("10.1.2.3", new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api0"));
+
+        InetAddress a = null;
+        InetAddress b = null;
+        InetAddress c = null;
+        try {
+            a = InetAddress.getByName("10.1.2.3");
+            b = InetAddress.getByName("10.1.2.4");
+            c = InetAddress.getByName("2001:4998:efeb:2ff::a:2");
+        } catch (UnknownHostException e) {
+            fail();
+        }
+
+        assertEquals(dynamicIps.getV4().size(), 1);
+        assertEquals(dynamicIps.getV6().size(), 0);
+        assertTrue(dynamicIps.getV4().containsKey(a));
+        assertFalse(dynamicIps.getV4().containsKey(b));
+
+        dynamicIps.putIp("2001:4998:efeb:2ff::a:2", new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api0"));
+
+        assertEquals(dynamicIps.getV4().size(), 1);
+        assertEquals(dynamicIps.getV6().size(), 1);
+        assertTrue(dynamicIps.getV4().containsKey(a));
+        assertTrue(dynamicIps.getV6().containsKey(c));
+    }
+
+    @Test
+    public void putIpInvalid() {
+        InetAddressMap<DynamicWorkload> dynamicIps = new InetAddressMap<>(new TreeMap<>(InetComparator::compare), new TreeMap<>(InetComparator::compare));
+
+        dynamicIps.putIp("10.1.2.a", new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api0"));
+
+        assertEquals(dynamicIps.getV4().size(), 0);
+        assertEquals(dynamicIps.getV6().size(), 0);
+    }
+
+    @Test
+    public void putIps() {
+        InetAddressMap<DynamicWorkload> dynamicIps = new InetAddressMap<>(new TreeMap<>(InetComparator::compare), new TreeMap<>(InetComparator::compare));
+
+        List<String> ips = Arrays.asList("10.1.2.3", "10.1.2.4", "2001:4998:efeb:2ff::a:2", "10.1.2.a");
+        dynamicIps.putIps(ips, new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api0"));
+
+        InetAddress a = null;
+        InetAddress b = null;
+        InetAddress c = null;
+        try {
+            a = InetAddress.getByName("10.1.2.3");
+            b = InetAddress.getByName("10.1.2.4");
+            c = InetAddress.getByName("2001:4998:efeb:2ff::a:2");
+        } catch (UnknownHostException e) {
+            fail();
+        }
+
+        assertEquals(dynamicIps.getV4().size(), 2);
+        assertEquals(dynamicIps.getV6().size(), 1);
+        assertTrue(dynamicIps.getV4().containsKey(a));
+        assertTrue(dynamicIps.getV4().containsKey(b));
+        assertTrue(dynamicIps.getV6().containsKey(c));
+    }
+}
---
+++ libs/java/server_common/src/test/java/com/yahoo/athenz/common/server/msd/net/InetComparatorTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright The Athenz Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yahoo.athenz.common.server.msd.net;
+
+import com.yahoo.athenz.msd.DynamicWorkload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandles;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.*;
+
+import static org.testng.Assert.*;
+
+public class InetComparatorTest {
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test
+    public void testCompare() throws UnknownHostException {
+        assertTrue(InetComparator.compare(InetAddress.getByName("10.1.2.3"), InetAddress.getByName("10.0.2.3")) > 0);
+        assertEquals(0, InetComparator.compare(InetAddress.getByName("10.1.2.3"), InetAddress.getByName("10.1.2.3")));
+        assertTrue(InetComparator.compare(InetAddress.getByName("10.1.2.3"), InetAddress.getByName("10.1.2.4")) < 0);
+        assertTrue(InetComparator.compare(InetAddress.getByName("10.255.2.3"), InetAddress.getByName("255.254.254.253")) < 0);
+    }
+
+    @Test
+    public void testTreeMap() throws UnknownHostException {
+        NavigableMap<InetAddress, DynamicWorkload> map = new TreeMap<>(InetComparator::compare);
+        map.put(InetAddress.getByName("12.1.3.1"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api0"));
+        map.put(InetAddress.getByName("10.1.2.3"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api1"));
+        map.put(InetAddress.getByName("10.1.2.4"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api2"));
+        map.put(InetAddress.getByName("10.1.3.1"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api31"));
+        map.put(InetAddress.getByName("10.1.3.2"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api32"));
+        map.put(InetAddress.getByName("11.1.3.1"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api4"));
+        map.put(InetAddress.getByName("10.2.3.1"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api5"));
+        map.put(InetAddress.getByName("10.2.3.2"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api6"));
+
+        String low = "10.1.2.0";
+        String high = "10.1.3.2";
+        SortedMap<InetAddress, DynamicWorkload> matched = map.subMap(InetAddress.getByName(low), InetAddress.getByName(high));
+        Set<InetAddress> matchedKeys = matched.keySet();
+        assertEquals(matchedKeys.size(), 3);
+        assertTrue(matchedKeys.contains(InetAddress.getByName("10.1.2.3")));
+        assertTrue(matchedKeys.contains(InetAddress.getByName("10.1.2.4")));
+        assertTrue(matchedKeys.contains(InetAddress.getByName("10.1.3.1")));
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testIncompatibleArgs() throws UnknownHostException {
+        NavigableMap<InetAddress, DynamicWorkload> map = new TreeMap<>(InetComparator::compare);
+        map.put(InetAddress.getByName("12.1.3.1"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api0"));
+        map.put(InetAddress.getByName("2001:4998:efeb:2ff::a:2"), new DynamicWorkload().setDomainName("athenz.examples").setServiceName("api1"));
+    }
+}
