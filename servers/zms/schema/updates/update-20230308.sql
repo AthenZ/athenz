bbd5a37be1846d1c680451fb1d3fffb6e73b712f
Henry Avetisyan <havetisy@yahoo.com>
2023-03-09 10:39:13 -0800

    update zms server schema to set tag key column with utf8-bin collation (#2094)
    Signed-off-by: Henry Avetisyan <hga@yahooinc.com>
    Co-authored-by: Henry Avetisyan <hga@yahooinc.com>

---
+++ servers/zms/schema/updates/update-20230308.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `zms_server`.`domain_tags` MODIFY `key` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL;
+ALTER TABLE `zms_server`.`role_tags` MODIFY `key` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL;
+ALTER TABLE `zms_server`.`group_tags` MODIFY `key` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL;
