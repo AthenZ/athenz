b56ddd2de2ad61f9d6d81cddefc43185f2e11b2e
jtsang01 <jimmy.tsang@yahooinc.com>
2023-03-18 19:23:00 -0700

    add login page
    Signed-off-by: jtsang01 <jimmy.tsang@yahooinc.com>

--- ui/src/redux/selectors/user.js
+++ ui/src/redux/selectors/user.js
@@ -16,5 +16,5 @@
 
 export const selectUserPendingMembers = (state) =>
     state.user.pendingMembers ? state.user.pendingMembers : [];
-    export const selectUserResourceAccessList = (state) =>
+export const selectUserResourceAccessList = (state) =>
     state.user.resourceAccessList ? state.user.resourceAccessList : [];
