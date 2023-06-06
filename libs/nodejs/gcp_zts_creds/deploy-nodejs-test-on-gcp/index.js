cf823d2fc15b653d6b1b7d5ffbfd3a2f7dd9b199
gilad.bendor <gilad.bendor@yahooinc.com>
2023-06-06 17:48:35 +0300

    Started working on NodeJS GCF tester

--- libs/nodejs/gcp_zts_creds/deploy-nodejs-test-on-gcp/index.js
+++ libs/nodejs/gcp_zts_creds/deploy-nodejs-test-on-gcp/index.js
@@ -2,6 +2,7 @@
 
 const functions = require('@google-cloud/functions-framework');
 const http = require("http");
+const https = require("https");
 const pem = require('pem');
 
 // See https://cloud.google.com/functions/docs/writing/write-http-functions#http-example-nodejs
@@ -18,37 +19,37 @@ async function getSiaCertsDemo() {
     const certOrg = "Oath"; // the dn you want included in cert - should not change
     const certDomain = "gcp.yahoo.cloud"; // do not change
 
-    const attestationData = await getGcpFunctionAttestationData(ztsUrl);
-    console.log(`GCP Attestation Data: ${attestationData}`);
-
-    // const privateKey = await generateRSAPrivateKey();
-    // console.log(`Private key constructor: `, privateKey.constructor.name);
-    // console.log(`Private key: `, privateKey);
-    // console.log(`Private key export: `, privateKey.export({ type: 'pkcs1', format: 'pem' }));
-
-    // TODO: We probably want to get reed of "info"...
-    const info = {
+    // TODO: We probably want to get reed of "ztsRequestBody"...
+    const ztsRequestBody = {
         domain: athenzDomain.toLowerCase(),
         service: athenzService.toLowerCase(),
         provider: athenzProvider.toLowerCase(),
     };
 
+    // Get an identity-document for this GCF from GCP.
+    ztsRequestBody.attestationData = await getGcpFunctionAttestationData(ztsUrl);
+    console.log(`GCP Attestation Data: ${ztsRequestBody.attestationData}`);
+
     // Create a CSR (and a private-key).
     const { privateKey, csr } = await generateCsr(
-        `${info.domain}.${info.service}`,
+        `${ztsRequestBody.domain}.${ztsRequestBody.service}`,
         certOrg,
         certOrgUnit,
         [
-            `${info.service}.${info.domain.replace(/\./g, '-')}.${certDomain}`,
-            `gcf-${gcpProjectId}-${info.service}.instanceid.athenz.${certDomain}`,
-            `spiffe://${info.domain}/sa/${info.service}`,
+            `${ztsRequestBody.service}.${ztsRequestBody.domain.replace(/\./g, '-')}.${certDomain}`,
+            `gcf-${gcpProjectId}-${ztsRequestBody.service}.instanceid.athenz.${certDomain}`,
+            `spiffe://${ztsRequestBody.domain}/sa/${ztsRequestBody.service}`,
         ]);
+    ztsRequestBody.csr = csr;
 
     console.log('CSR:\n', csr);
     console.log('Private Key:\n', privateKey);
 
+    // Send CSR to ZTS.
+    const ztsResponse = await getCredsFromZts(ztsUrl, ztsRequestBody);
+    console.log(`ZTS response: ${JSON.stringify(ztsResponse, null, 4)}`);
 
-    return "<<<CERT>>>";
+    return ztsResponse.x509Certificate;
 }
 
 // Get an identity-document for this GCF from GCP.
@@ -66,12 +67,13 @@ function getGcpFunctionAttestationData(ztsUrl) {
             (res) => {
                 // Read response body.
                 res.setEncoding('utf8');
-                let responseBody = '';
+                let responseBodyChunks = [];
                 res.on('data', (chunk) => {
-                    responseBody += chunk;
+                    responseBodyChunks.push(chunk);
                 });
                 res.on('end', () => {
                     // Response is read successfully.
+                    const responseBody = responseBodyChunks.join('');
                     if (res.statusCode !== 200) {
                         // Bad HTTP status code.
                         reject(new Error(`HTTP request to    ${gcpIdentityUrl}    returned ${res.statusCode} (${res.status}). Body:\n${responseBody}`));
@@ -111,6 +113,51 @@ function generateCsr(commonName, organization, organizationalUnit, altNames) {
             }));
 }
 
+
+// Send CSR to ZTS.
+function getCredsFromZts(ztsUrl, ztsRequestBody) {
+    return new Promise((resolve, reject) => {
+        const requestUrl = `${ztsUrl}/instance`;
+        const request = https.request(
+            requestUrl,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            },
+            (response) => {
+                response.setEncoding('utf8');
+
+                const responseBodyChunks = [];
+                response.on('data', (chunk) => {
+                    responseBodyChunks.push(chunk);
+                });
+
+                response.on('end', () => {
+                    // Response is read successfully.
+                    const responseBody = responseBodyChunks.join('');
+                    if (response.statusCode !== 200) {
+                        // Bad HTTP status code.
+                        reject(new Error(`HTTP request to    ${requestUrl}    returned ${response.statusCode} (${response.status}). Body:\n${responseBody}`));
+                    } else {
+                        // Parse response.
+                        try {
+                            const response = JSON.parse(responseBody);
+                            resolve(response);
+                        } catch (error) {
+                            reject(new Error(`HTTP request to    ${requestUrl}    returned non-JSON response. Body:\n${responseBody}`));
+                        }
+                    }
+                });
+            });
+        request.on('error', (error) => {
+            reject(new Error(`Failed to make HTTP request to    ${requestUrl}    : ${error}`));
+        });
+        request.write(JSON.stringify(ztsRequestBody));
+        request.end()
+    });
+}
 
 
 
