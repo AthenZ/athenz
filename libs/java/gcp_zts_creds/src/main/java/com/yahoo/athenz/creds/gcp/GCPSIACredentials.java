b08fa43a5656e2e8e6b7b7c12a692c26053ba355
gilad.bendor <gilad.bendor@yahooinc.com>
2023-06-14 08:56:00 +0300

    Testers moved to athenz-examples
    Signed-off-by: gilad.bendor <gilad.bendor@yahooinc.com>

--- libs/java/gcp_zts_creds/src/main/java/com/yahoo/athenz/creds/gcp/GCPSIACredentials.java
+++ libs/java/gcp_zts_creds/src/main/java/com/yahoo/athenz/creds/gcp/GCPSIACredentials.java
@@ -15,8 +15,11 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.bouncycastle.asn1.DERIA5String;
 import org.bouncycastle.asn1.x509.GeneralName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -25,14 +28,23 @@ import java.security.cert.X509Certificate;
 
 public class GCPSIACredentials {
 
+    // Configurations.
+    public static int ZTS_CONNECT_TIMEOUT_MS = 5000;
+    public static int ZTS_READ_TIMEOUT_MS = 30000;
+    public static int ATTESTATION_CONNECT_TIMEOUT_MS = ZTS_CONNECT_TIMEOUT_MS;
+    public static int ATTESTATION_READ_TIMEOUT_MS = ZTS_READ_TIMEOUT_MS;
+
+    /** Response of {@link #getGCPFunctionServiceCertificate} */
     public static class PrivateAndCertificate {
         public X509Certificate certificate;
-        public PrivateKey privateKey;
-        public String caCertificates;
+        public String          certificatePem;
+        public PrivateKey      privateKey;
+        public String          privateKeyPem;
+        public String          caCertificatesPem;
     }
 
     /**
-     * For GCP cloud-functions generate a new private key, request a
+     * For GCP cloud-functions generate a new private key, request an
      * x.509 certificate based on the requested CSR and return both to
      * the client in order to establish tls connections with other
      * Athenz enabled services.
@@ -40,6 +52,13 @@ public class GCPSIACredentials {
      * @param athenzService name of the service
      * @param gcpProjectId GCP project-id that the function runs in
      * @param athenzProvider name of the provider service for GCP Cloud-Functions
+     * @param ztsUrl Something like: https://...:.../zts/v1
+     * @param certDomain TODO: Abhijeet - explain what this is...
+     * @param optionalCountry Optional field in the certificate's Subject.
+     * @param optionalState Optional field in the certificate's Subject.
+     * @param optionalLocality Optional field in the certificate's Subject.
+     * @param optionalOrganization Optional field in the certificate's Subject.
+     * @param optionalOrganizationUnit Optional field in the certificate's Subject.
      * @return GCPFunctionIdentity with private key and certificate
      */
     public static PrivateAndCertificate getGCPFunctionServiceCertificate(
@@ -78,6 +97,7 @@ public class GCPSIACredentials {
         // Generate a private-key.
         PrivateAndCertificate response = new PrivateAndCertificate();
         response.privateKey = Crypto.generateRSAPrivateKey(2048);
+        response.privateKeyPem = Crypto.convertToPEMFormat(response.privateKey);
 
         // Build the Alternative DNS names (SAN's).
         GeneralName[] sanArray = buildAlternativeDnsNames(
@@ -100,8 +120,9 @@ public class GCPSIACredentials {
                 attestationData,
                 csr);
 
+        response.certificatePem = identity.x509Certificate;
         response.certificate = Crypto.loadX509Certificate(identity.x509Certificate);
-        response.caCertificates = identity.x509CertificateSigner;
+        response.caCertificatesPem = identity.x509CertificateSigner;
         return response;
     }
 
@@ -136,18 +157,21 @@ public class GCPSIACredentials {
 
     /** Get GCP attestation data for GCP Function. */
     private static String getGcpFunctionAttestationData(String ztsUrl) throws Exception {
-        String gcpIdentityUrl = "http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=" + ztsUrl + "&format=full";
-        // LOG.debug("Getting GCF identity from: {}", gcpIdentityUrl);
+        String gcpIdentityUrl = ATTESTATION_DATA_URL_PREFIX + ztsUrl;
+        // LOG.debug("Getting GCF attestation-data from: {}", gcpIdentityUrl);
         HttpURLConnection httpConnection = null;
         try {
             httpConnection = (HttpURLConnection) new URL(gcpIdentityUrl).openConnection();
             httpConnection.setRequestMethod("GET");
             httpConnection.setRequestProperty("Metadata-Flavor", "Google");
+            httpConnection.setConnectTimeout(ATTESTATION_CONNECT_TIMEOUT_MS);
+            httpConnection.setReadTimeout(ATTESTATION_READ_TIMEOUT_MS);
             int status = httpConnection.getResponseCode();
             String identityToken = new String(httpConnection.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
             if (status != 200) {
                 throw new Exception("Unable to generate GCF attestation data from URL \"" + gcpIdentityUrl + "\" : HTTP code " + status + " != 200");
             }
+            // LOG.debug("GCF attestation-data: {}", identityToken);
             return "{\"identityToken\":\"" + identityToken + "\"}";
         } catch (IOException exception) {
             throw new Exception("Unable to generate GCF attestation data from URL \"" + gcpIdentityUrl + "\" : ", exception);
@@ -189,8 +213,8 @@ public class GCPSIACredentials {
 
         // Construct an HTTP client.
         RequestConfig config = RequestConfig.custom()
-                .setConnectTimeout(DEFAULT_CLIENT_CONNECT_TIMEOUT_MS)
-                .setSocketTimeout(DEFAULT_CLIENT_READ_TIMEOUT_MS)
+                .setConnectTimeout(ZTS_CONNECT_TIMEOUT_MS)
+                .setSocketTimeout(ZTS_READ_TIMEOUT_MS)
                 .setRedirectsEnabled(false)
                 .build();
         try (CloseableHttpClient httpClient = HttpClients.custom()
@@ -198,15 +222,15 @@ public class GCPSIACredentials {
                 .build()) {
 
             // Construct an HTTP POST request.
-            HttpEntity httpEntity = new StringEntity(
-                    "{" +
-                            "\"domain\": " + OBJECT_MAPPER.writeValueAsString(athenzDomain) + "," +
-                            "\"service\": " + OBJECT_MAPPER.writeValueAsString(athenzService) + "," +
-                            "\"provider\": " + OBJECT_MAPPER.writeValueAsString(athenzProvider) + "," +
-                            "\"attestationData\": " + OBJECT_MAPPER.writeValueAsString(attestationData) + "," +
-                            "\"csr\": " + OBJECT_MAPPER.writeValueAsString(csr) +
-                            "}",
-                    ContentType.APPLICATION_JSON);
+            String postPayload = "{" +
+                    "\"domain\": " + OBJECT_MAPPER.writeValueAsString(athenzDomain) + "," +
+                    "\"service\": " + OBJECT_MAPPER.writeValueAsString(athenzService) + "," +
+                    "\"provider\": " + OBJECT_MAPPER.writeValueAsString(athenzProvider) + "," +
+                    "\"attestationData\": " + OBJECT_MAPPER.writeValueAsString(attestationData) + "," +
+                    "\"csr\": " + OBJECT_MAPPER.writeValueAsString(csr) +
+                    "}";
+            // LOG.debug("Getting identity from ZTS: requesting \"{}/instance\" with payload: {}", ztsUrl, postPayload);
+            HttpEntity httpEntity = new StringEntity(postPayload, ContentType.APPLICATION_JSON);
             HttpUriRequest httpUriRequest = RequestBuilder.post()
                     .setUri(ztsUrl + "/instance")
                     .setEntity(httpEntity)
@@ -218,8 +242,10 @@ public class GCPSIACredentials {
             try (CloseableHttpResponse httpResponse = httpClient.execute(httpUriRequest)) {
                 int statusCode = httpResponse.getStatusLine().getStatusCode();
                 httpResponseEntity = httpResponse.getEntity();
-                if (statusCode == 201) {
-                    return OBJECT_MAPPER.readValue(httpResponseEntity.getContent(), InstanceIdentity.class);
+                if ((statusCode == 200) || (statusCode == 201)) {
+                    InstanceIdentity response = OBJECT_MAPPER.readValue(httpResponseEntity.getContent(), InstanceIdentity.class);
+                    // LOG.debug("Got identity from ZTS:    x509Certificate={}    x509CertificateSigner={}", OBJECT_MAPPER.writeValueAsString(response.x509Certificate), OBJECT_MAPPER.writeValueAsString(response.x509CertificateSigner));
+                    return response;
                 } else {
                     final String errorBody = (httpResponseEntity == null) ? "<no response body>" : EntityUtils.toString(httpResponseEntity);
                     throw new Exception("Unable to register instance with Athenz. HTTP status: " + statusCode + ". Response: " + errorBody);
@@ -230,13 +256,15 @@ public class GCPSIACredentials {
         }
     }
 
+    /** Used to parse ZTS response */
     @JsonIgnoreProperties(ignoreUnknown = true)
     private static class InstanceIdentity {
         public String x509Certificate;
         public String x509CertificateSigner;
     }
 
+    static String ATTESTATION_DATA_URL_PREFIX = "http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?format=full&audience=";
+
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final int DEFAULT_CLIENT_CONNECT_TIMEOUT_MS = 5000;
-    private static final int DEFAULT_CLIENT_READ_TIMEOUT_MS = 30000;
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 }
