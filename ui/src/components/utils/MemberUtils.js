3ec7ef3ffeba089f0c4b4e83aee1c1c5f44aad25
dvirguttman <dvirguttman@gmail.com>
2023-08-25 01:29:30 +0300

    Add search capability for Add Group component (#2280)
    * Add search capability for Add Group component
    When user is not found - add it as the single value to be selected
    
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>
    
    * lint errors
    
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>
    
    ---------
    
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>
    Co-authored-by: Dvir Guttman <dvir.guttman@verizonmedia.com>

--- ui/src/components/utils/MemberUtils.js
+++ ui/src/components/utils/MemberUtils.js
@@ -52,6 +52,12 @@ export default class MemberUtils {
                     value: USER_DOMAIN + '.' + u.login,
                 })
             );
+            if (usersArr.length === 0) {
+                usersArr.push({
+                    name: part,
+                    value: part,
+                });
+            }
             return usersArr;
         });
     }
@@ -67,7 +73,9 @@ export default class MemberUtils {
                             `[(\\s]${escapedPrefix}|^${escapedPrefix}`,
                             'i'
                         ).test(userData.name) ||
-                        new RegExp(`^${escapedPrefix}`, 'i').test(userData.login)
+                        new RegExp(`^${escapedPrefix}`, 'i').test(
+                            userData.login
+                        )
                     ) {
                         return { ...userData, score: 1 };
                     }
@@ -83,6 +91,5 @@ export default class MemberUtils {
                 .sort((a, b) => b.score - a.score)
                 .slice(0, 10)
         );
-    };
-
+    }
 }
--- ui/src/tests_utils/ComponentsTestUtils.js
+++ ui/src/tests_utils/ComponentsTestUtils.js
@@ -195,3 +195,11 @@ export const mockAllDomainDataApiCalls = (domainDetails, headerDetails) => {
 export const getStateWithUserList = (userList, initialState = {}) => {
     return { ...initialState, user: userList };
 };
+
+export const getStateWithDomainDataAndUserList = (
+    domainData,
+    userList,
+    initialState = {}
+) => {
+    return { ...initialState, domainData: domainData, user: userList };
+};
