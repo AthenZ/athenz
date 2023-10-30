2ab8603e5ff598762e17378a539244bd8e8d1f2c
Abhijeet V <31417623+abvaidya@users.noreply.github.com>
2023-10-29 19:25:30 -0700

    replacing wix-embedded-mysql with testcontainers-java
    Signed-off-by: Abhijeet V <31417623+abvaidya@users.noreply.github.com>

--- screwdriver/scripts/install_deps.sh
+++ screwdriver/scripts/install_deps.sh
@@ -13,8 +13,14 @@ echo "-----------------Install maven: -----------------"
 apt-get install -y maven
 
 echo "-----------------Install nodejs: -----------------"
-curl -sL https://deb.nodesource.com/setup_18.x | bash -
-apt-get install -y nodejs
+apt-get update
+apt-get install -y ca-certificates curl gnupg apt-transport-https
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
 aptitude install -y npm
 npm install -g npm@latest
 
@@ -29,6 +35,18 @@ wget https://golang.org/dl/go1.19.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
+echo "-----------------Install Docker: -----------------"
+# Add Docker's official GPG key:
+apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+docker system info
+ls -la $SD_DIND_SHARE_PATH
+
 # check all installed dependencies
 echo "-----------------Java Version: -----------------"
 java -version
@@ -40,3 +58,5 @@ echo "-----------------NPM Version: -----------------"
 npm -v
 echo "-----------------Golang Version: -----------------"
 go version
+echo "-----------------Docker Version: -----------------"
+docker version
--- servers/zms/src/test/java/com/yahoo/athenz/zms/ZMSBinderTest.java
+++ servers/zms/src/test/java/com/yahoo/athenz/zms/ZMSBinderTest.java
@@ -1,7 +1,7 @@
 package com.yahoo.athenz.zms;
 
-import com.wix.mysql.EmbeddedMysql;
 import com.yahoo.athenz.auth.impl.FilePrivateKeyStore;
+import org.testcontainers.containers.MySQLContainer;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,14 +11,14 @@ public class ZMSBinderTest {
     private static final String DB_USER = "admin";
     private static final String DB_PASS = "unit-test";
 
-    private EmbeddedMysql mysqld;
+    private MySQLContainer<?> mysqld;
 
     @BeforeClass
     public void setUp() {
         mysqld = ZMSTestUtils.startMemoryMySQL(DB_USER, DB_PASS);
 
         System.setProperty(ZMSConsts.ZMS_PROP_OBJECT_STORE_FACTORY_CLASS, "com.yahoo.athenz.zms.store.impl.JDBCObjectStoreFactory");
-        System.setProperty(ZMSConsts.ZMS_PROP_JDBC_RW_STORE, "jdbc:mysql://localhost:3310/zms_server");
+        System.setProperty(ZMSConsts.ZMS_PROP_JDBC_RW_STORE, mysqld.getJdbcUrl());
         System.setProperty(ZMSConsts.ZMS_PROP_JDBC_RW_USER, DB_USER);
         System.setProperty(ZMSConsts.ZMS_PROP_JDBC_RW_PASSWORD, DB_PASS);
         System.setProperty(ZMSConsts.ZMS_PROP_PRINCIPAL_STATE_UPDATER_DISABLE_TIMER, "true");
