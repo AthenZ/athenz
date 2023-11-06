b6fe63a7702559bcf7a9c085c5cc60235a6b7a8a
Raja0623 <rskanchanapalli97@gmail.com>
2023-11-06 17:58:39 -0500

    Final Commit
    Signed-off-by: Raja0623 <rskanchanapalli97@gmail.com>

--- ui/src/components/domain/ManageDomains.js
+++ ui/src/components/domain/ManageDomains.js
@@ -379,9 +379,12 @@ class ManageDomains extends React.Component {
                           <TDStyled color={color} align={center}>
                               {item.domain.account ? item.domain.account : ''}
                           </TDStyled>
+                          <TDStyled color={color} align={center}>
+                            {item.domain.gcpProject}
+                          </TDStyled>   
                           <TDStyledBusinessService
                               color={color}
-                              align={left}
+                              align={center}
                               title={title}
                           >
                               <StyledAnchor onClick={businessServiceItem}>
@@ -400,6 +403,9 @@ class ManageDomains extends React.Component {
                                       id={item.domain.name + '-delete-button'}
                                   />
                               ) : null}
+                          </TDStyled>
+                          <TDStyled color={color} align={center}>
+                              {item.domain.account ? item.domain.account : ''}
                           </TDStyled>
                       </TrStyled>
                   );
@@ -475,6 +481,9 @@ class ManageDomains extends React.Component {
                             </TableHeadStyled>
                             <TableHeadStyled align={center}>
                                 AWS Account #
+                            </TableHeadStyled>
+                            <TableHeadStyled align={center}>
+                                GCP Project ID
                             </TableHeadStyled>
                             <TableHeadStyled align={left}>
                                 Business Service
