f90327d0fd9f939aee2c07cb2f276cdf1d947d11
Shimaoka Shuya <sshimaok@lycorp.co.jp>
2023-11-10 15:50:19 +0900

    add assy dir
    Signed-off-by: Shimaoka Shuya <sshimaok@lycorp.co.jp>

--- docker/env.sh
+++ docker/env.sh
@@ -3,6 +3,8 @@
 set -u
 set -o pipefail
 
+export ATHENZ_TAG=$(sed -n -e 's|<version>\(.*SNAPSHOT\)</version>|\1|p' ${BASE_DIR}/pom.xml | tr -d ' ')
+
 # Base dirs
 export BASE_DIR="$(git rev-parse --show-toplevel)"
 export DOCKER_DIR="${BASE_DIR}/docker"
@@ -18,6 +20,7 @@ export ZMS_LOGS_DIR="${DOCKER_DIR}/logs/zms"
 export ZMS_CONF_DIR="${ZMS_DIR}/conf"
 export ZMS_CERTS_DIR="${ZMS_DIR}/var/certs"
 export ZMS_KEYS_DIR="${ZMS_DIR}/var/keys"
+export ZMS_ASSY_DIR="${BASE_DIR}/assembly/zms/target/athenz-zms-${ATHENZ_TAG}"
 mkdir -p "${ZMS_LOGS_DIR}"
 mkdir -p "${ZMS_CONF_DIR}"
 mkdir -p "${ZMS_CERTS_DIR}"
@@ -43,6 +46,7 @@ export ZTS_LOGS_DIR="${DOCKER_DIR}/logs/zts"
 export ZTS_CONF_DIR="${ZTS_DIR}/conf"
 export ZTS_CERTS_DIR="${ZTS_DIR}/var/certs"
 export ZTS_KEYS_DIR="${ZTS_DIR}/var/keys"
+export ZTS_ASSY_DIR="${BASE_DIR}/assembly/zts/target/athenz-zts-${ATHENZ_TAG}"
 mkdir -p "${ZTS_LOGS_DIR}"
 mkdir -p "${ZTS_CONF_DIR}"
 mkdir -p "${ZTS_CERTS_DIR}"
