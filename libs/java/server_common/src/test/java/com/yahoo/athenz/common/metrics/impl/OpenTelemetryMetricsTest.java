06835f224c6eba54769a6a3144a1c252f3f3b822
salladi <sahiti.alladi@yahooinc.com>
2024-06-21 10:46:48 -0700

    OpenTelemetryMetric.java & pom changes
    Signed-off-by: salladi <sahiti.alladi@yahooinc.com>

---
+++ libs/java/server_common/src/test/java/com/yahoo/athenz/common/metrics/impl/OpenTelemetryMetricsTest.java
@@ -0,0 +1,14 @@
+package com.yahoo.athenz.common.metrics.impl;
+
+import static org.mockito.Mockito.*;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+
+import org.mockito.ArgumentCaptor;
+
+public class OpenTelemetryMetricsTest {
+}
