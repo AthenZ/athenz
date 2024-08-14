112cb7c77e4c9f5223996b44667f9dd75b3898c9
aporss <art.porss@yahooinc.com>
2024-08-14 11:48:43 +0100

    ATHENS-7451 - fix red role review icon conditions
    Signed-off-by: aporss <art.porss@yahooinc.com>

--- ui/src/__tests__/components/role/RoleRow.test.js
+++ ui/src/__tests__/components/role/RoleRow.test.js
@@ -14,15 +14,19 @@
  * limitations under the License.
  */
 import React from 'react';
-import RoleRow from '../../../components/role/RoleRow';
+import RoleRow, { getSmallestExpiryOrReview, isReviewRequired } from '../../../components/role/RoleRow';
+import { _ } from 'lodash';
 import { colors } from '../../../components/denali/styles';
 import { renderWithRedux } from '../../../tests_utils/ComponentsTestUtils';
-import { fireEvent, screen, waitFor } from '@testing-library/react';
-import { configure } from '@testing-library/dom';
-import { act } from 'react-dom/test-utils';
-import { USER_DOMAIN } from '../../../components/constants/constants';
+import { fireEvent, screen  } from '@testing-library/react';
+import moment from 'moment';
 
-describe('RoleRow', () => {
+describe('RoleRow', (object, method) => {
+
+    afterAll(() => {
+        jest.clearAllMocks();
+    });
+
     it('should render', () => {
         const details = {
             name: 'athens:role.ztssia_cert_rotate',
@@ -78,5 +82,107 @@ describe('RoleRow', () => {
         expect(descriptionIcon).toBeInTheDocument();
         fireEvent.mouseEnter(descriptionIcon);
         await screen.findByText('test description');
+    });
+
+    it('getSmallestExpiryOrReview check memberExpiryDays value is picked up', async () => {
+        const role = {memberExpiryDays: 5, lastReviewedDate: '2024-07-24T10:58:07.533Z'};
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(role.memberExpiryDays, actualDays)).toBeTruthy();
+    });
+
+    it('getSmallestExpiryOrReview check serviceExpiryDays value is picked up', async () => {
+        const role = {serviceExpiryDays: 9, lastReviewedDate: '2024-07-24T10:58:07.533Z'};
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(role.serviceExpiryDays, actualDays)).toBeTruthy();
+    });
+
+    it('getSmallestExpiryOrReview check memberReviewDays value is picked up', async () => {
+        const role = {memberReviewDays: 6, lastReviewedDate: '2024-07-24T10:58:07.533Z'};
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(role.memberReviewDays, actualDays)).toBeTruthy();
+    });
+
+    it('getSmallestExpiryOrReview check serviceReviewDays value is picked up', async () => {
+        const role = {serviceReviewDays: 10, lastReviewedDate: '2024-07-24T10:58:07.533Z'};
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(role.serviceReviewDays, actualDays)).toBeTruthy();
+    });
+
+    it('getSmallestExpiryOrReview check groupReviewDays value is picked up', async () => {
+        const role = {groupReviewDays: 8, lastReviewedDate: '2024-07-24T10:58:07.533Z'};
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(role.groupReviewDays, actualDays)).toBeTruthy();
+    });
+
+    it('getSmallestExpiryOrReview check groupExpiryDays value is picked up', async () => {
+        const role = {groupExpiryDays: 7, lastReviewedDate: '2024-07-24T10:58:07.533Z'};
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(role.groupExpiryDays, actualDays)).toBeTruthy();
+    });
+
+    it("getSmallestExpiryOrReview check smallest value picked, non 0, null doesn't produce error", async () => {
+        const role = {
+            memberExpiryDays: 0,
+            serviceExpiryDays: null,
+            memberReviewDays: 6,
+            serviceReviewDays: 10,
+            groupReviewDays: 8,
+            groupExpiryDays: 7,
+            lastReviewedDate: '2024-07-24T10:58:07.533Z'
+        };
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(role.memberReviewDays, actualDays)).toBeTruthy();
+    });
+
+    it("getSmallestExpiryOrReview when no expiry or review days assigned to the role, return 0", async () => {
+        const role = {
+            lastReviewedDate: '2024-07-24T10:58:07.533Z'
+        };
+        let actualDays = getSmallestExpiryOrReview(role);
+
+        expect(_.isEqual(0, actualDays)).toBeTruthy();
+    });
+
+    it("isReviewRequired when no expiry or review days assigned to the role, return false", async () => {
+        const role = {
+        };
+        let reviewRequired = isReviewRequired(role);
+
+        expect(_.isEqual(false, reviewRequired)).toBeTruthy();
+    });
+
+    it("isReviewRequired when lastReviewDate is more than 80% of memberExpiryDate ago - return true", async () => {
+        const role = {
+            lastReviewedDate: '2024-07-01T11:59:59.000Z',
+            memberExpiryDays: '10'
+        };
+        // mock current date
+        jest.spyOn(moment.prototype, 'utc')
+            .mockReturnValue(moment('2024-07-09T12:00:00.000Z').utc());
+
+        let reviewRequired = isReviewRequired(role);
+
+        expect(_.isEqual(true, reviewRequired)).toBeTruthy();
+    });
+
+    it("isReviewRequired when lastReviewDate is less than 80% of memberExpiryDate ago - return false", async () => {
+        const role = {
+            lastReviewedDate: '2024-07-01T12:00:01.000Z',
+            memberExpiryDays: '10'
+        };
+        // mock current date
+        // jest.spyOn(moment.prototype, 'utc')
+        //     .mockReturnValue(moment('2024-07-09T12:00:00.000Z').utc());
+
+        let reviewRequired = isReviewRequired(role);
+
+        expect(_.isEqual(false, reviewRequired)).toBeTruthy();
     });
 });
