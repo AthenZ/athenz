bc41c4f3e8e129e0032eef24bb7cce45899f8a6a
YuChen Wang <yuchen.wang@yahooinc.com>
2025-03-18 10:06:09 -0700

    access_log2
    Signed-off-by: YuChen Wang <yuchen.wang@yahooinc.com>

--- ui/src/server/handlers/logger.js
+++ ui/src/server/handlers/logger.js
@@ -27,21 +27,17 @@ module.exports = function (expressApp, config) {
                     method: req.method,
                     host: req.headers.host,
                     path: req.originalUrl,
-                    user:
-                        req.okta && req.okta.claims && req.okta.claims.short_id
-                            ? req.okta.claims.short_id
-                            : 'invalid user',
-                    remoteIP:
-                        req.headers['y-ra'] ||
-                        req.headers['yahooremoteip'] ||
-                        req.connection.remoteAddress,
+                    user: req.session.shortId
+                        ? req.session.shortId
+                        : 'invalid user',
+                    remoteIP: req.connection.remoteAddress,
                     statusCode: res.statusCode,
                     bytesWritten:
                         req.connection.bytesWritten -
                         (req.connection.lastBytesWritten || 0),
                 })
             );
-            
+
             req.connection.lastBytesWritten = req.connection.bytesWritten;
             if (req.body && req.body.requests) {
                 Object.values(req.body.requests).forEach((call) => {
