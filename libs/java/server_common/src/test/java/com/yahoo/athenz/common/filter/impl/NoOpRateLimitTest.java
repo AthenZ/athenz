e2c0dbe4ba8ba34166e07eb2d1ce69847b0498d0
Henry Avetisyan <hga@yahooinc.com>
2025-05-16 15:18:10 -0700

    add test case for ratelimit default method
    Signed-off-by: Henry Avetisyan <hga@yahooinc.com>

--- libs/java/server_common/src/test/java/com/yahoo/athenz/common/filter/impl/NoOpRateLimitTest.java
+++ libs/java/server_common/src/test/java/com/yahoo/athenz/common/filter/impl/NoOpRateLimitTest.java
@@ -1,6 +1,9 @@
 package com.yahoo.athenz.common.filter.impl;
 
+import com.yahoo.athenz.common.filter.RateLimit;
 import com.yahoo.athenz.common.metrics.Metric;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
 
@@ -21,5 +24,12 @@ public class NoOpRateLimitTest {
         Metric metric = Mockito.mock(Metric.class);
         assertFalse(noOpRateLimitFactory.create().filter(new MockHttpServletRequest(), new MockHttpServletResponse()));
         assertFalse(noOpRateLimitFactory.create().filter(new MockHttpServletRequest(), new MockHttpServletResponse(), metric));
+    }
+
+    @Test
+    public void testRateLimit() {
+        RateLimit rateLimit = (servletRequest, servletResponse) -> false;
+        assertFalse(rateLimit.filter(new MockHttpServletRequest(), new MockHttpServletResponse()));
+        assertFalse(rateLimit.filter(new MockHttpServletRequest(), new MockHttpServletResponse(), Mockito.mock(Metric.class)));
     }
 }
