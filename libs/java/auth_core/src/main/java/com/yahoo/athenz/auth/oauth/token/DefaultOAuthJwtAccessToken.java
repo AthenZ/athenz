b9f19c866a0ce542ef29671936e315fd47ee5120
yosrixp <yosrixp@hotmail.com>
2025-06-13 07:47:13 -0700

    Add support to a different client_id jwt claim name (#2996)
    * Add support to a different client_id jwt claim name
    
    Signed-off-by: yosrixp <yosrixp@yahooinc.com>
    
    * Apply code review changes, Ignore test case
    
    Signed-off-by: yosrixp <yosrixp@yahooinc.com>
    
    Signed-off-by: yosrixp <yosrixp@yahooinc.com>
    
    ---------
    
    Signed-off-by: yosrixp <yosrixp@yahooinc.com>
    Co-authored-by: yosrixp <yosrixp@yahooinc.com>

--- libs/java/auth_core/src/main/java/com/yahoo/athenz/auth/oauth/token/DefaultOAuthJwtAccessToken.java
+++ libs/java/auth_core/src/main/java/com/yahoo/athenz/auth/oauth/token/DefaultOAuthJwtAccessToken.java
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 public class DefaultOAuthJwtAccessToken implements OAuthJwtAccessToken {
 
     private static final Logger LOG = LoggerFactory.getLogger(DefaultOAuthJwtAccessToken.class);
+    private static final String JWT_CLIENT_ID_CLAIM_NAME = System.getProperty("athenz.auth.jwt.claim.client_id.name", OAuthJwtAccessToken.CLAIM_CLIENT_ID);
 
     protected JWTClaimsSet claimsSet;
 
@@ -75,7 +76,7 @@ public class DefaultOAuthJwtAccessToken implements OAuthJwtAccessToken {
     @Override
     public String getClientId() {
         try {
-            return claimsSet.getStringClaim(OAuthJwtAccessToken.CLAIM_CLIENT_ID);
+            return claimsSet.getStringClaim(JWT_CLIENT_ID_CLAIM_NAME);
         } catch (Exception ex) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("DefaultOAuthJwtAccessToken:getClientId expected data type to be string, err: {}", ex.getMessage());
