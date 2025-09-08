847f8786d15fa067d4a18942c329921068560d5b
Sasidhar <psasidhar@gmail.com>
2025-09-08 07:51:08 -0700

    Separating GCP SyncLauncher into its own project under "syncers" (#3067)
    Signed-off-by: Sasi Palaka <palakas@yahooinc.com>
    Co-authored-by: Sasi Palaka <palakas@yahooinc.com>

--- syncers/zms_gcp_domain_syncer/src/test/java/io/athenz/syncer/gcp/SyncHelperTest.java
+++ syncers/zms_gcp_domain_syncer/src/test/java/io/athenz/syncer/gcp/SyncHelperTest.java
@@ -1,24 +1,24 @@
 /*
+ * Copyright The Athenz Authors
  *
- *  * Copyright The Athenz Authors
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *     http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package com.yahoo.athenz.zms_gcp_domain_syncer;
+package io.athenz.syncer.gcp;
 
 import io.athenz.syncer.common.zms.CloudZmsSyncer;
 import io.athenz.syncer.common.zms.Config;
+import io.athenz.syncer.gcp.common.impl.GcsDomainStoreFactory;
+import io.athenz.syncer.gcp.common.impl.GcsStateFileBuilderFactory;
 import org.testng.annotations.Test;
 
 import java.util.Objects;
@@ -27,7 +27,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.*;
 
-public class GcpZmsSyncerHelperTest {
+public class SyncHelperTest {
     private final ClassLoader classLoader = this.getClass().getClassLoader();
 
     @Test(expectedExceptions = Exception.class)
@@ -35,7 +35,7 @@ public class GcpZmsSyncerHelperTest {
         System.setProperty(Config.PROP_PREFIX + Config.SYNC_CFG_PARAM_ROOT_PATH, "/invalid-path");
         System.setProperty(CloudZmsSyncer.SYNC_PROP_CLOUD_DOMAIN_STORE_FACTORY_CLASS, "io.athenz.syncer.common.zms.impl.IdontExist");
         try {
-            assertNotNull(new GcpZmsSyncerHelper().createCloudZmsSyncer());
+            assertNotNull(new SyncHelper().createCloudZmsSyncer());
         } finally {
             System.clearProperty(Config.PROP_PREFIX + Config.SYNC_CFG_PARAM_ROOT_PATH);
             System.clearProperty(CloudZmsSyncer.SYNC_PROP_STATEFILE_BUILDER_FACTORY_CLASS);
@@ -61,7 +61,7 @@ public class GcpZmsSyncerHelperTest {
         Config.getInstance().loadConfigParams();
 
         try {
-            assertNotNull(new GcpZmsSyncerHelper().createCloudZmsSyncer());
+            assertNotNull(new SyncHelper().createCloudZmsSyncer());
         } finally {
             System.clearProperty(Config.PROP_PREFIX + Config.SYNC_CFG_PARAM_ROOT_PATH);
             System.clearProperty(Config.PROP_PREFIX + Config.SYNC_CFG_PARAM_STATE_PATH);
@@ -81,7 +81,7 @@ public class GcpZmsSyncerHelperTest {
         when(cloudZmsSyncer.processDomains()).thenReturn(true);
 
         // Create a test subclass that overrides createCloudZmsSyncer
-        GcpZmsSyncerHelper helper = new GcpZmsSyncerHelper() {
+        SyncHelper helper = new SyncHelper() {
             @Override
             protected CloudZmsSyncer createCloudZmsSyncer() throws Exception {
                 return cloudZmsSyncer;
@@ -95,7 +95,7 @@ public class GcpZmsSyncerHelperTest {
     public void testRunFailure() throws Exception {
         CloudZmsSyncer cloudZmsSyncer = mock(CloudZmsSyncer.class);
         when(cloudZmsSyncer.processDomains()).thenReturn(false);
-        GcpZmsSyncerHelper helper = new GcpZmsSyncerHelper() {
+        SyncHelper helper = new SyncHelper() {
             @Override
             protected CloudZmsSyncer createCloudZmsSyncer() throws Exception {
                 return cloudZmsSyncer;
@@ -109,7 +109,7 @@ public class GcpZmsSyncerHelperTest {
     @Test
     public void testRunException() {
         // Create a test subclass that overrides createCloudZmsSyncer
-        GcpZmsSyncerHelper helper = new GcpZmsSyncerHelper() {
+        SyncHelper helper = new SyncHelper() {
             @Override
             protected CloudZmsSyncer createCloudZmsSyncer() {
                 throw new RuntimeException("Test exception");
@@ -118,5 +118,53 @@ public class GcpZmsSyncerHelperTest {
 
         // Test the run method
         assertFalse(helper.run());
+    }
+
+    @Test
+    public void testSetRequiredProperties() {
+        final String domainStoreProp = CloudZmsSyncer.SYNC_PROP_CLOUD_DOMAIN_STORE_FACTORY_CLASS;
+        final String stateFileProp = CloudZmsSyncer.SYNC_PROP_STATEFILE_BUILDER_FACTORY_CLASS;
+        String originalDomainStoreProp = System.getProperty(domainStoreProp);
+        String originalStateFileProp = System.getProperty(stateFileProp);
+
+        try {
+            // Clear properties first to ensure a clean state
+            System.clearProperty(CloudZmsSyncer.SYNC_PROP_CLOUD_DOMAIN_STORE_FACTORY_CLASS);
+            System.clearProperty(CloudZmsSyncer.SYNC_PROP_STATEFILE_BUILDER_FACTORY_CLASS);
+
+            // Test when properties are not set
+            SyncHelper helper = new SyncHelper();
+            helper.setRequiredProperties();
+
+            // Verify properties were set correctly
+            assertEquals(System.getProperty(CloudZmsSyncer.SYNC_PROP_CLOUD_DOMAIN_STORE_FACTORY_CLASS),
+                    GcsDomainStoreFactory.class.getName());
+            assertEquals(System.getProperty(CloudZmsSyncer.SYNC_PROP_STATEFILE_BUILDER_FACTORY_CLASS),
+                    GcsStateFileBuilderFactory.class.getName());
+
+            // Test when properties are already set
+            System.setProperty(CloudZmsSyncer.SYNC_PROP_CLOUD_DOMAIN_STORE_FACTORY_CLASS, "custom.factory.class");
+            System.setProperty(CloudZmsSyncer.SYNC_PROP_STATEFILE_BUILDER_FACTORY_CLASS, "custom.builder.class");
+
+            helper.setRequiredProperties();
+
+            // Verify properties remain unchanged
+            assertEquals(System.getProperty(CloudZmsSyncer.SYNC_PROP_CLOUD_DOMAIN_STORE_FACTORY_CLASS),
+                    "custom.factory.class");
+            assertEquals(System.getProperty(CloudZmsSyncer.SYNC_PROP_STATEFILE_BUILDER_FACTORY_CLASS),
+                    "custom.builder.class");
+        } finally {
+            // Clean up and restore original properties
+            if (originalDomainStoreProp != null) {
+                System.setProperty(domainStoreProp, originalDomainStoreProp);
+            } else {
+                System.clearProperty(domainStoreProp);
+            }
+            if (originalStateFileProp != null) {
+                System.setProperty(stateFileProp, originalStateFileProp);
+            } else {
+                System.clearProperty(stateFileProp);
+            }
+        }
     }
 }
