66185a4da253ee3a444e1989247aa4db6a105a57
msharma07 <mukesh.sharma@yahooinc.com>
2025-12-16 11:52:26 +0530

    config file updated for otel
    Signed-off-by: msharma07 <mukesh.sharma@yahooinc.com>

--- ui/pm2.config.js
+++ ui/pm2.config.js
@@ -26,7 +26,7 @@ module.exports = {
             node_args: '-r ./src/opentelemetry/instrumentation.mjs',
             env_production: {
                 NODE_ENV: 'production',
-                OTEL_EXPORTER_OTLP_CA_CERTIFICATE: process.env.OTEL_EXPORTER_CA_CERTIFICATE,
+                OTEL_EXPORTER_OTLP_CA_CERTIFICATE: process.env.OTEL_EXPORTER_OTLP_CA_CERTIFICATE,
             },
         },
     ],
