0e9b938a63630efdc3bbb23a9807ec891c69e531
Henry Avetisyan <hga@yahooinc.com>
2026-02-05 16:51:20 -0800

    extensible issuer support for tokens
    Signed-off-by: Henry Avetisyan <hga@yahooinc.com>

--- servers/zts/src/main/java/com/yahoo/athenz/zts/ZTSConsts.java
+++ servers/zts/src/main/java/com/yahoo/athenz/zts/ZTSConsts.java
@@ -75,6 +75,7 @@ public final class ZTSConsts {
     public static final String ZTS_PROP_REDIRECT_URI_SUFFIX    = "athenz.zts.redirect_uri_suffix";
     public static final String ZTS_PROP_SCOPE_ROLE_WOUT_DOMAIN = "athenz.zts.oauth_scope_role_without_domain";
     public static final String ZTS_PROP_PROVIDER_CONFIG_FILE   = "athenz.zts.oauth_provider_config_file";
+    public static final String ZTS_PROP_HOST_ISSUER_MAPPING_FILE = "athenz.zts.host_issuer_mapping_file";
 
     public static final String ZTS_PROP_CERTSIGN_BASE_URI            = "athenz.zts.certsign_base_uri";
     public static final String ZTS_PROP_CERTSIGN_REQUEST_TIMEOUT     = "athenz.zts.certsign_request_timeout";
