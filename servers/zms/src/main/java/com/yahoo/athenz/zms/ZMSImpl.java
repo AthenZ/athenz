fa4b8c10a5c7a3e3d6c361bbda12ba5e185d6393
Henry Avetisyan <hga@yahooinc.com>
2026-03-28 18:29:34 -0700

    extract the external domain prefix before calling validatMember method
    Signed-off-by: Henry Avetisyan <hga@yahooinc.com>

--- servers/zms/src/main/java/com/yahoo/athenz/zms/ZMSImpl.java
+++ servers/zms/src/main/java/com/yahoo/athenz/zms/ZMSImpl.java
@@ -4896,11 +4896,25 @@ public class ZMSImpl implements Authorizer, KeyStore, ZMSHandler {
 
             case EXTERNAL:
 
-                externalMemberValidatorManager.validateMember(domainName, memberName, caller);
+                validateExternalMember(domainName, memberName, caller);
                 break;
         }
 
         return userType;
+    }
+
+    void validateExternalMember(final String domainName, final String memberName, final String caller) {
+
+        // we need to separate the external member prefix from the member name
+        // and only pass the member name to the external resource validator
+        // we'll check for the missing separator though we should never get there
+        // since the server has already validated the format of the external member name
+
+        int sepIdx = memberName.indexOf(AuthorityConsts.EXT_SEP);
+        if (sepIdx == -1) {
+            throw ZMSUtils.requestError("Principal " + memberName + " is not valid", caller);
+        }
+        externalMemberValidatorManager.validateMember(domainName, memberName.substring(sepIdx + AuthorityConsts.EXT_SEP.length()), caller);
     }
 
     Authority.UserType validateGroupMemberPrincipal(final String domainName, final String groupName,
@@ -4949,8 +4963,8 @@ public class ZMSImpl implements Authorizer, KeyStore, ZMSHandler {
 
             case EXTERNAL:
 
-                externalMemberValidatorManager.validateMember(domainName, memberName, caller);
-                break;
+            validateExternalMember(domainName, memberName, caller);
+            break;
         }
 
         return userType;
